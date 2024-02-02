1c220f289e21a23e8737013d10d696b82587058f
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-02 22:25:51 +0100

    test

--- src/events/client/hello.ts
+++ src/events/client/hello.ts
@@ -3,6 +3,7 @@ import { LogType } from "../../types";
 import { logger } from "../../util/util";
 
 export default new Event("hello", false, (interval: number, shard: number) => {
+  console.log("hello");
   logger(
     `Shard ${shard} received hello opcode with an interval of ${interval}`,
     LogType.Info,
