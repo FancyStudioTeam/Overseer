d7ed0061b5c746276c8b91108a4f0c93f355d543
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-07 21:08:05 +0100

    yey

---
+++ src/components/buttons/suggestions/suggestions-general-channel.ts
@@ -0,0 +1,70 @@
+import {
+  ChannelTypes,
+  type ComponentInteraction,
+  ComponentTypes,
+} from "oceanic.js";
+import { ActionRowBuilder } from "../../../builders/ActionRow";
+import { SelectMenuBuilder } from "../../../builders/SelectMenu";
+import { Component } from "../../../classes/Builders";
+import type { Fancycord } from "../../../classes/Client";
+import { prisma } from "../../../util/db";
+import { errorMessage } from "../../../util/util";
+
+export default new Component({
+  name: "suggestions-general-channel",
+  permissions: {
+    user: "MANAGE_GUILD",
+  },
+  run: async (
+    client: Fancycord,
+    interaction: ComponentInteraction,
+    { language },
+  ) => {
+    await interaction.deferUpdate().catch(() => null);
+
+    if (!interaction.guild) {
+      return errorMessage(interaction, true, {
+        description: client.locales.__({
+          phrase: "general.cannot-get-guild",
+          locale: language,
+        }),
+      });
+    }
+
+    const guildSuggestion = await prisma.guildSuggestion.findUnique({
+      where: {
+        guild_id: interaction.guild.id,
+      },
+    });
+
+    if (!guildSuggestion) {
+      return errorMessage(interaction, true, {
+        description: client.locales.__({
+          phrase: "commands.configuration.suggestions.system-not-enabled",
+          locale: language,
+        }),
+      });
+    }
+
+    await interaction.message.edit({
+      components: new ActionRowBuilder()
+        .addComponents([
+          new SelectMenuBuilder()
+            .setCustomID("suggestions-general-channel")
+            .setType(ComponentTypes.CHANNEL_SELECT)
+            .setChannelTypes([ChannelTypes.GUILD_TEXT])
+            .setDefaultValues(
+              interaction.guild.channels.get(guildSuggestion.channel_id)
+                ? [
+                    {
+                      id: guildSuggestion.channel_id,
+                      type: "channel",
+                    },
+                  ]
+                : [],
+            ),
+        ])
+        .toJSONArray(),
+    });
+  },
+});
---
+++ src/components/buttons/suggestions/suggestions-general.ts
@@ -0,0 +1,226 @@
+import { Prisma } from "@prisma/client";
+import {
+  ButtonStyles,
+  type ComponentInteraction,
+  type Message,
+} from "oceanic.js";
+import { ActionRowBuilder } from "../../../builders/ActionRow";
+import { ButtonBuilder } from "../../../builders/Button";
+import { EmbedBuilder } from "../../../builders/Embed";
+import { Component } from "../../../classes/Builders";
+import type { Fancycord } from "../../../classes/Client";
+import { prisma } from "../../../util/db";
+import { errorMessage } from "../../../util/util";
+
+export default new Component({
+  name: "suggestions-general",
+  permissions: {
+    user: "MANAGE_GUILD",
+  },
+  run: async (
+    client: Fancycord,
+    interaction: ComponentInteraction,
+    { language, premium },
+  ) => {
+    await interaction.deferUpdate().catch(() => null);
+
+    if (!interaction.guild) {
+      return errorMessage(interaction, true, {
+        description: client.locales.__({
+          phrase: "general.cannot-get-guild",
+          locale: language,
+        }),
+      });
+    }
+
+    const guildSuggestion = await prisma.guildSuggestion.findUnique({
+      where: {
+        guild_id: interaction.guild.id,
+      },
+    });
+
+    if (!guildSuggestion) {
+      return errorMessage(interaction, true, {
+        description: client.locales.__({
+          phrase: "commands.configuration.suggestions.system-not-enabled",
+          locale: language,
+        }),
+      });
+    }
+
+    await updateGeneralMessage(
+      {
+        client,
+        language,
+        premium,
+      },
+      guildSuggestion,
+      interaction.message,
+    );
+  },
+});
+
+export async function updateGeneralMessage(
+  main: {
+    client: Fancycord;
+    language: string;
+    premium?: boolean;
+  },
+  data: Prisma.GuildSuggestionCreateInput | null,
+  message: Message,
+): Promise<void> {
+  await message.edit({
+    embeds: new EmbedBuilder()
+      .setAuthor({
+        name: message.guild?.name as string,
+        iconURL: message.guild?.iconURL() ?? main.client.user.avatarURL(),
+      })
+      .setThumbnail(message.guild?.iconURL() ?? main.client.user.avatarURL())
+      .addFields([
+        {
+          name: main.client.locales.__({
+            phrase:
+              "commands.configuration.suggestions.row.general.message.field",
+            locale: main.language,
+          }),
+          value: main.client.locales.__mf(
+            {
+              phrase:
+                "commands.configuration.suggestions.row.general.message.value",
+              locale: main.language,
+            },
+            {
+              channel:
+                message.guild?.channels.get(data?.channel_id ?? "")?.mention ??
+                "<:_:1201586248947597392>",
+              messageSuggestion: data?.message_suggestions
+                ? "<:_:1203774493936455730>"
+                : "<:_:1203774491763937300>",
+              threads: data?.threads
+                ? "<:_:1203774493936455730>"
+                : "<:_:1203774491763937300>",
+            },
+          ),
+        },
+        {
+          name: main.client.locales.__({
+            phrase:
+              "commands.configuration.suggestions.row.general.message.field2",
+            locale: main.language,
+          }),
+          value: main.client.locales.__mf(
+            {
+              phrase:
+                "commands.configuration.suggestions.row.general.message.value2",
+              locale: main.language,
+            },
+            {
+              reviewChannel:
+                message.guild?.channels.get(data?.revision_channel_id ?? "")
+                  ?.mention ?? "<:_:1201586248947597392>",
+              review: data?.revision_enabled
+                ? "<:_:1203774493936455730>"
+                : "<:_:1203774491763937300>",
+            },
+          ),
+        },
+      ])
+      .setColor(main.client.config.colors.color)
+      .toJSONArray(),
+    components: [
+      new ActionRowBuilder()
+        .addComponents([
+          new ButtonBuilder()
+            .setCustomID("suggestions-general-channel")
+            .setLabel(
+              main.client.locales.__({
+                phrase:
+                  "commands.configuration.suggestions.row.general.row.channel.label",
+                locale: main.language,
+              }),
+            )
+            .setStyle(ButtonStyles.SECONDARY)
+            .setEmoji({
+              name: "_",
+              id: "1203801953088442418",
+            }),
+          new ButtonBuilder()
+            .setCustomID("suggestions-general-message")
+            .setLabel(
+              main.client.locales.__({
+                phrase:
+                  "commands.configuration.suggestions.row.general.row.message.label",
+                locale: main.language,
+              }),
+            )
+            .setStyle(
+              data?.message_suggestions
+                ? ButtonStyles.SUCCESS
+                : ButtonStyles.SECONDARY,
+            )
+            .setEmoji({
+              name: "_",
+              id: data?.message_suggestions
+                ? "1203774493936455730"
+                : "1203774491763937300",
+            }),
+          new ButtonBuilder()
+            .setCustomID("suggestions-general-threads")
+            .setLabel(
+              main.client.locales.__({
+                phrase:
+                  "commands.configuration.suggestions.row.general.row.threads.label",
+                locale: main.language,
+              }),
+            )
+            .setStyle(
+              data?.threads ? ButtonStyles.SUCCESS : ButtonStyles.SECONDARY,
+            )
+            .setEmoji({
+              name: "_",
+              id: data?.threads ? "1203774493936455730" : "1203774491763937300",
+            })
+            .setDisabled(!main.premium),
+        ])
+        .toJSON(),
+      new ActionRowBuilder()
+        .addComponents([
+          new ButtonBuilder()
+            .setCustomID("suggestions-general-revision-channel")
+            .setLabel(
+              main.client.locales.__({
+                phrase:
+                  "commands.configuration.suggestions.row.general.row.revision-channel.label",
+                locale: main.language,
+              }),
+            )
+            .setStyle(ButtonStyles.SECONDARY)
+            .setEmoji({
+              name: "_",
+              id: "1203801953088442418",
+            }),
+          new ButtonBuilder()
+            .setCustomID("suggestions-general-revision")
+            .setLabel(
+              main.client.locales.__({
+                phrase:
+                  "commands.configuration.suggestions.row.general.row.revision.label",
+                locale: main.language,
+              }),
+            )
+            .setStyle(
+              data?.revision_enabled
+                ? ButtonStyles.SUCCESS
+                : ButtonStyles.SECONDARY,
+            )
+            .setEmoji({
+              name: "_",
+              id: data?.revision_enabled
+                ? "1203774493936455730"
+                : "1203774491763937300",
+            }),
+        ])
+        .toJSON(),
+    ],
+  });
+}
