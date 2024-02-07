2a5e50ada811a8445f16d9272cf74391e39552ae
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-07 21:06:49 +0100

    update this

---
+++ src/components/buttons/suggestions/suggestions-enable-disable.ts
@@ -0,0 +1,66 @@
+import { Prisma } from "@prisma/client";
+import type { ComponentInteraction } from "oceanic.js";
+import { Component } from "../../../classes/Builders";
+import type { Fancycord } from "../../../classes/Client";
+import { updateMainMessage } from "../../../commands/chatInput/config/subcommands/suggestions";
+import { prisma } from "../../../util/db";
+import { errorMessage } from "../../../util/util";
+
+export default new Component({
+  name: "suggestions-enable-disable",
+  permissions: {
+    user: "MANAGE_GUILD",
+  },
+  run: async (
+    client: Fancycord,
+    interaction: ComponentInteraction,
+    { language },
+  ) => {
+    await interaction.deferUpdate().catch(() => null);
+
+    if (!interaction.guild) {
+      return errorMessage(interaction, true, {
+        description: client.locales.__({
+          phrase: "general.cannot-get-guild",
+          locale: language,
+        }),
+      });
+    }
+
+    const guildSuggestion = await prisma.guildSuggestion.findUnique({
+      where: {
+        guild_id: interaction.guild.id,
+      },
+    });
+    let newData: Prisma.GuildSuggestionCreateInput | null;
+
+    if (guildSuggestion) {
+      newData = await prisma.guildSuggestion
+        .delete({
+          where: {
+            guild_id: guildSuggestion.guild_id,
+          },
+        })
+        .then(() => {
+          return null;
+        });
+    } else {
+      newData = await prisma.guildSuggestion.create({
+        data: {
+          guild_id: interaction.guild.id,
+          channel_id: "",
+          revision_channel_id: "",
+        },
+      });
+    }
+
+    await updateMainMessage(
+      {
+        client,
+        language,
+      },
+      newData,
+      interaction.message,
+    );
+  },
+});
