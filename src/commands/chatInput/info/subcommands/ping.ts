3e4bbe6639289ea73e55ffa77e3cc4a276992e59
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-10 17:23:50 +0100

    update ping

--- src/commands/chatInput/info/subcommands/ping.ts
+++ src/commands/chatInput/info/subcommands/ping.ts
@@ -2,19 +2,26 @@ import type { CommandInteraction } from "oceanic.js";
 import { EmbedBuilder } from "../../../../builders/Embed";
 import { SubCommand } from "../../../../classes/Builders";
 import type { Fancycord } from "../../../../classes/Client";
-import { formatString } from "../../../../util/util";
 
 export default new SubCommand({
   name: "ping",
   run: async (client: Fancycord, interaction: CommandInteraction) => {
     interaction.reply({
       embeds: new EmbedBuilder()
-        .setDescription(
-          `\`\`\`ansi\n${formatString(
-            `REST ∷ ${client.rest.handler.latencyRef.latency} ms\nShard ∷ ${interaction.guild?.shard.latency} ms`,
-            "∷",
-          )}\`\`\``,
-        )
+        .addFields([
+          {
+            name: "**REST**",
+            value: `<:_:1201948012830531644> ${client.rest.handler.latencyRef.latency} ms`,
+            inline: true,
+          },
+          {
+            name: "**WS**",
+            value: `<:_:1201948012830531644> ${
+              interaction.guild?.shard.latency ?? 0
+            } ms`,
+            inline: true,
+          },
+        ])
         .setColor(client.config.colors.color)
         .toJSONArray(),
     });
