bf5a32f58994d4257883726f37299fef60ca6dc6
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-17 18:52:01 +0100

    add type

--- src/commands/chatInput/config/subcommands/suggestions.ts
+++ src/commands/chatInput/config/subcommands/suggestions.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 import { ButtonStyles, type CommandInteraction } from "oceanic.js";
 import { ActionRowBuilder } from "../../../../builders/ActionRow";
 import { ButtonBuilder } from "../../../../builders/Button";
