362745be899a84e739d00ac486650586567d4d40
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-18 16:34:06 +0100

    update suggestion module

--- src/modules/suggestions.ts
+++ src/modules/suggestions.ts
@@ -1,15 +1,107 @@
 import { DiscordSnowflake } from "@sapphire/snowflake";
 import { ButtonStyles, ChannelTypes, type Message } from "oceanic.js";
+import { RateLimiterMemory } from "rate-limiter-flexible";
 import { ActionRowBuilder } from "../builders/ActionRow";
 import { ButtonBuilder } from "../builders/Button";
 import { EmbedBuilder } from "../builders/Embed";
 import type { Fancycord } from "../classes/Client";
 import { prisma } from "../util/db";
-import { cleanContent, sleep, trim } from "../util/util";
+import { cleanContent, consume, sleep, trim } from "../util/util";
+
+const suggestionRateLimiter = new RateLimiterMemory({
+  points: 1,
+  duration: 30,
+  blockDuration: 60,
+});
+export const buttons = (client: Fancycord, language: string) =>
+  new ActionRowBuilder()
+    .addComponents([
+      new ButtonBuilder()
+        .setCustomID("suggest-upvote")
+        .setLabel("0")
+        .setStyle(ButtonStyles.SECONDARY)
+        .setEmoji({
+          name: "_",
+          id: "1201583878205353994",
+        }),
+      new ButtonBuilder()
+        .setCustomID("suggest-downvote")
+        .setLabel("0")
+        .setStyle(ButtonStyles.SECONDARY)
+        .setEmoji({
+          name: "_",
+          id: "1201583875424538675",
+        }),
+      new ButtonBuilder()
+        .setCustomID("suggest-manage")
+        .setStyle(ButtonStyles.SECONDARY)
+        .setEmoji({
+          name: "_",
+          id: "1201584289473630208",
+        }),
+      new ButtonBuilder()
+        .setCustomID("suggest-report")
+        .setLabel(
+          client.locales.__({
+            phrase: "commands.utility.suggest.row.report.label",
+            locale: language,
+          }),
+        )
+        .setStyle(ButtonStyles.DANGER)
+        .setEmoji({
+          name: "_",
+          id: "1201583419524657315",
+        }),
+    ])
+    .toJSONArray();
+export const revision = (client: Fancycord, language: string) =>
+  new ActionRowBuilder()
+    .addComponents([
+      new ButtonBuilder()
+        .setCustomID("suggest-status-approve")
+        .setLabel(
+          client.locales.__({
+            phrase: "commands.utility.suggest.row.status.approve.label",
+            locale: language,
+          }),
+        )
+        .setStyle(ButtonStyles.SECONDARY)
+        .setEmoji({
+          name: "_",
+          id: "1201582315915190312",
+        }),
+      new ButtonBuilder()
+        .setCustomID("suggest-status-deny")
+        .setLabel(
+          client.locales.__({
+            phrase: "commands.utility.suggest.row.status.deny.label",
+            locale: language,
+          }),
+        )
+        .setStyle(ButtonStyles.SECONDARY)
+        .setEmoji({
+          name: "_",
+          id: "1201581407290531890",
+        }),
+      new ButtonBuilder()
+        .setCustomID("suggest-report")
+        .setLabel(
+          client.locales.__({
+            phrase: "commands.utility.suggest.row.report.label",
+            locale: language,
+          }),
+        )
+        .setStyle(ButtonStyles.DANGER)
+        .setEmoji({
+          name: "_",
+          id: "1201583419524657315",
+        }),
+    ])
+    .toJSONArray();
 
 export default (client: Fancycord) => {
   client.on("messageCreate", async (message: Message) => {
-    if (!message.guild) return;
+    if (!message.inCachedGuildChannel() || !message.guild) return;
     if (!message.channel) return;
     if (message.channel.type !== ChannelTypes.GUILD_TEXT) return;
     if (message.author.bot) return;
@@ -32,11 +124,17 @@ export default (client: Fancycord) => {
       message.channel.type === ChannelTypes.GUILD_TEXT &&
       guildSuggestion.channel_id === message.channel.id
     ) {
+      const rateLimit = await consume(message.author.id, suggestionRateLimiter);
+
+      if (rateLimit.rateLimited) return;
+
       const id = DiscordSnowflake.generate().toString();
       let content = message.content;
 
       if (message.guild.clientMember.permissions.has("MANAGE_MESSAGES")) {
-        await message.delete().catch(() => null);
+        await client.rest.channels
+          .deleteMessage(message.channelID, message.id)
+          .catch(() => null);
       }
 
       if (!checkString(content)) {
@@ -48,79 +146,60 @@ export default (client: Fancycord) => {
           guildSuggestion.revision_channel_id,
         );
 
+        if (!channel) {
+          return await client.rest.users
+            .createDM(message.author.id)
+            .then(async (newChannel) => {
+              await client.rest.channels
+                .createMessage(newChannel.id, {
+                  embeds: new EmbedBuilder()
+                    .setDescription(
+                      client.locales.__({
+                        phrase:
+                          "commands.configuration.suggestions.row.general.row.revision.revision-not-found",
+                        locale: language,
+                      }),
+                    )
+                    .setColor(client.config.colors.error)
+                    .toJSONArray(),
+                })
+                .catch(() => null);
+            })
+            .catch(() => null);
+        }
+
         if (
-          channel &&
           channel.type === ChannelTypes.GUILD_TEXT &&
           channel
             .permissionsOf(message.guild.clientMember)
             .has("VIEW_CHANNEL", "SEND_MESSAGES")
         ) {
-          const response = await channel.createMessage({
-            embeds: [
-              new EmbedBuilder()
-                .setDescription(
-                  client.locales.__({
-                    phrase: "commands.utility.suggest.message2",
-                    locale: language,
-                  }),
-                )
-                .setColor(client.config.colors.warning)
-                .toJSON(),
-              new EmbedBuilder()
-                .setAuthor({
-                  name: message.author.username,
-                  iconURL: message.author.avatarURL(),
-                })
-                .setThumbnail(message.author.avatarURL())
-                .setDescription(cleanContent(trim(content, 4000)))
-                .setColor(client.config.colors.color)
-                .toJSON(),
-            ],
-            components: new ActionRowBuilder()
-              .addComponents([
-                new ButtonBuilder()
-                  .setCustomID("suggest-status-approve")
-                  .setLabel(
+          const response = await client.rest.channels.createMessage(
+            channel.id,
+            {
+              embeds: [
+                new EmbedBuilder()
+                  .setDescription(
                     client.locales.__({
-                      phrase:
-                        "commands.utility.suggest.row.status.approve.label",
+                      phrase: "commands.utility.suggest.message2",
                       locale: language,
                     }),
                   )
-                  .setStyle(ButtonStyles.SECONDARY)
-                  .setEmoji({
-                    name: "_",
-                    id: "1201582315915190312",
-                  }),
-                new ButtonBuilder()
-                  .setCustomID("suggest-status-deny")
-                  .setLabel(
-                    client.locales.__({
-                      phrase: "commands.utility.suggest.row.status.deny.label",
-                      locale: language,
-                    }),
-                  )
-                  .setStyle(ButtonStyles.SECONDARY)
-                  .setEmoji({
-                    name: "_",
-                    id: "1201581407290531890",
-                  }),
-                new ButtonBuilder()
-                  .setCustomID("suggest-report")
-                  .setLabel(
-                    client.locales.__({
-                      phrase: "commands.utility.suggest.row.report.label",
-                      locale: language,
-                    }),
-                  )
-                  .setStyle(ButtonStyles.DANGER)
-                  .setEmoji({
-                    name: "_",
-                    id: "1201583419524657315",
-                  }),
-              ])
-              .toJSONArray(),
-          });
+                  .setColor(client.config.colors.warning)
+                  .toJSON(),
+                new EmbedBuilder()
+                  .setAuthor({
+                    name: message.author.username,
+                    iconURL: message.author.avatarURL(),
+                  })
+                  .setThumbnail(message.author.avatarURL())
+                  .setDescription(cleanContent(trim(content, 4000)))
+                  .setColor(client.config.colors.color)
+                  .toJSON(),
+              ],
+              components: revision(client, language),
+            },
+          );
 
           await prisma.userSuggestion.create({
             data: {
@@ -135,62 +214,30 @@ export default (client: Fancycord) => {
         }
       } else {
         if (
-          message.channel.type === ChannelTypes.GUILD_TEXT &&
           message.channel
             .permissionsOf(message.guild.clientMember)
-            .has("VIEW_CHANNEL", "SEND_MESSAGES")
+            .has(
+              "VIEW_CHANNEL",
+              "SEND_MESSAGES",
+              "EMBED_LINKS",
+              "USE_EXTERNAL_EMOJIS",
+            )
         ) {
-          const response = await message.channel.createMessage({
-            embeds: new EmbedBuilder()
-              .setAuthor({
-                name: message.author.username,
-                iconURL: message.author.avatarURL(),
-              })
-              .setThumbnail(message.author.avatarURL())
-              .setDescription(cleanContent(trim(content, 4000)))
-              .setColor(client.config.colors.color)
-              .toJSONArray(),
-            components: new ActionRowBuilder()
-              .addComponents([
-                new ButtonBuilder()
-                  .setCustomID("suggest-upvote")
-                  .setLabel("0")
-                  .setStyle(ButtonStyles.SECONDARY)
-                  .setEmoji({
-                    name: "_",
-                    id: "1201583878205353994",
-                  }),
-                new ButtonBuilder()
-                  .setCustomID("suggest-downvote")
-                  .setLabel("0")
-                  .setStyle(ButtonStyles.SECONDARY)
-                  .setEmoji({
-                    name: "_",
-                    id: "1201583875424538675",
-                  }),
-                new ButtonBuilder()
-                  .setCustomID("suggest-manage")
-                  .setStyle(ButtonStyles.SECONDARY)
-                  .setEmoji({
-                    name: "_",
-                    id: "1201584289473630208",
-                  }),
-                new ButtonBuilder()
-                  .setCustomID("suggest-report")
-                  .setLabel(
-                    client.locales.__({
-                      phrase: "commands.utility.suggest.row.report.label",
-                      locale: language,
-                    }),
-                  )
-                  .setStyle(ButtonStyles.DANGER)
-                  .setEmoji({
-                    name: "_",
-                    id: "1201583419524657315",
-                  }),
-              ])
-              .toJSONArray(),
-          });
+          const response = await client.rest.channels.createMessage(
+            message.channelID,
+            {
+              embeds: new EmbedBuilder()
+                .setAuthor({
+                  name: message.author.username,
+                  iconURL: message.author.avatarURL(),
+                })
+                .setThumbnail(message.author.avatarURL())
+                .setDescription(cleanContent(trim(content, 4000)))
+                .setColor(client.config.colors.color)
+                .toJSONArray(),
+              components: buttons(client, language),
+            },
+          );
 
           await prisma.userSuggestion.create({
             data: {
@@ -205,12 +252,10 @@ export default (client: Fancycord) => {
 
           if (guildSuggestion.threads && premium) {
             await sleep(1500);
-            await response
-              .startThread({
+            await client.rest.channels
+              .startThreadFromMessage(message.channelID, response.id, {
                 name: `${message.author.username} suggestion`,
                 autoArchiveDuration: 1440,
-                reason:
-                  'The suggestion system has the "threads" option enabled',
               })
               .catch(() => null);
           }
@@ -220,7 +265,7 @@ export default (client: Fancycord) => {
   });
 };
 
-function checkString(content: string): boolean {
+export function checkString(content: string): boolean {
   if (!content || !content.length) return false;
 
   return !/^[^a-zA-Z0-9]+$/.test(content);
