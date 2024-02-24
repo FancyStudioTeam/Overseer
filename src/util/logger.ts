d47eec6c2400c4ccf4ee3713b7fb7d54aebfb499
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-24 15:04:10 +0100

    true to false

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -30,7 +30,7 @@ export const logger = winston.createLogger({
       return `[${formatTimestamp(
         timestamp,
         "Europe/Madrid",
-        true
+        false
       )}] [${level}] ${message}`;
     })
   ),
