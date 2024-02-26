682de93fcd4bf477403a431a592cd4a6e31c5353
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-02-26 22:33:07 +0100

    Add "editStatus" method

--- src/events/shard/shardReady.ts
+++ src/events/shard/shardReady.ts
@@ -1,6 +1,17 @@
+import { ActivityTypes } from "oceanic.js";
+import { client } from "../..";
+import { version } from "../../../package.json";
 import { Event } from "../../classes/Builders";
 import { logger } from "../../util/logger";
 
 export default new Event("shardReady", false, async (id: number) => {
+  client.editStatus("online", [
+    {
+      name: ".",
+      state: `🍃 Version ${version}`,
+      type: ActivityTypes.CUSTOM,
+    },
+  ]);
+
   logger.log("INF", `[Shard ${id}] Shard has been connected`);
 });
