27b77337dff36ab4e8d3c4f9f23adf66200da441
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-05-26 12:50:12 +0200

    Add locales files

---
+++ src/locales/Locales/English.ts
@@ -0,0 +1,266 @@
+import colors from "@colors/colors";
+import { type Nullish, codeBlock } from "@sapphire/utilities";
+import { isNullOrUndefined } from "@sapphire/utilities";
+import { Emojis } from "#constants";
+import { padding } from "#util";
+
+export default {
+  COMMANDS: {
+    CONFIG: {
+      LANGUAGE: {
+        MESSAGE_1: `**${Emojis.SUCCESS} The language has been set to \`English\`**`,
+      },
+      PREMIUM: {
+        CLAIM: {
+          MEMBERSHIP_NOT_FOUND: ({ code }: { code: string }) =>
+            `**${Emojis.MARK} The code \`${code}\` has not been found**`,
+          MESSAGE_1: ({ expireDate }: { expireDate: string | Nullish }) =>
+            `**${Emojis.SUCCESS} The premium membership has been claimed**\n${
+              Emojis.RIGHT
+            } The premium membership will ${
+              isNullOrUndefined(expireDate)
+                ? "`never expire`"
+                : `expire on \`${expireDate}\``
+            }`,
+        },
+        REVOKE: {
+          COMPONENTS: {
+            BUTTONS: {
+              CONFIRM: {
+                LABEL: "Confirm",
+                MESSAGE_1: `**${Emojis.SUCCESS} The premium membership has been revoked**`,
+              },
+              CANCEL: {
+                LABEL: "Cancel",
+                MESSAGE_1: `**${Emojis.SUCCESS} The revocation has been cancelled**`,
+              },
+            },
+          },
+          INVALID_GUILD_MEMBERSHIP: `**${Emojis.MARK} The server does not have a premium membership**`,
+          MESSAGE_1: `**${Emojis.WARNING} You are about to cancel the premium membership**\n${Emojis.RIGHT} If you do so, you will no longer be able to enjoy the benefits of membership`,
+        },
+      },
+      TIMEZONE: {
+        ERRORS: {
+          TIMEZONE_NOT_FOUND: ({ timezone }: { timezone: string }) =>
+            `**${Emojis.MARK} The timezone \`${timezone}\` has not been found**`,
+        },
+        MESSAGE_1: ({ timezone }: { timezone: string }) =>
+          `**${Emojis.SUCCESS} The timezone has been set to \`${timezone}\`**`,
+      },
+    },
+    INFO: {
+      BOT: {
+        MESSAGE_1: {
+          TITLE_1: ({ name }: { name: string }) => `**${name} Information**`,
+          FIELD_1: {
+            FIELD: "**General Information**",
+            VALUE: ({ version, memory }: { version: string; memory: string }) =>
+              [
+                `${Emojis.RIGHT} **Version**: ${version}`,
+                `${Emojis.RIGHT} **RAM Usage**: ${memory}`,
+              ].join("\n"),
+          },
+          FIELD_2: {
+            FIELD: "**Statistics**",
+            VALUE: ({
+              users,
+              guilds,
+              shards,
+            }: {
+              users: number;
+              guilds: number;
+              shards: number;
+            }) =>
+              [
+                `${Emojis.RIGHT} **Users**: ${users} users`,
+                `${Emojis.RIGHT} **Servers**: ${guilds} servers`,
+                `${Emojis.RIGHT} **Shards**: ${shards} shards`,
+              ].join("\n"),
+          },
+          FIELD_3: {
+            FIELD: "**Last Reset Date**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+        },
+      },
+      PING: {
+        MESSAGE_1: {
+          DESCRIPTION_1: ({ rest, shard }: { rest: string; shard: string }) =>
+            codeBlock(
+              "ansi",
+              padding(
+                `${colors.reset.cyan("REST Reference")} - ${colors.bold.magenta(
+                  rest,
+                )}\n${colors.reset.cyan(
+                  "WebSocket Connection",
+                )} - ${colors.bold.magenta(shard)}`,
+                "-",
+              ),
+            ),
+        },
+      },
+      SERVER: {
+        MESSAGE_1: {
+          TITLE_1: ({ name }: { name: string }) => `**${name} Information**`,
+          FIELD_1: {
+            FIELD: "**General Information**",
+            VALUE: ({
+              name,
+              id,
+              owner,
+            }: {
+              name: string;
+              id: string;
+              owner: string;
+            }) =>
+              [
+                `${Emojis.RIGHT} **Name**: ${name}`,
+                `${Emojis.RIGHT} **ID**: ${id}`,
+                `${Emojis.RIGHT} **Owner**: ${owner}`,
+              ].join("\n"),
+          },
+          FIELD_2: {
+            FIELD: "**Statistics**",
+            VALUE: ({
+              members,
+              channels,
+              roles,
+            }: {
+              members: number;
+              channels: number;
+              roles: number;
+            }) =>
+              [
+                `${Emojis.RIGHT} **Members**: ${members} members`,
+                `${Emojis.RIGHT} **Channels**: ${channels} channels`,
+                `${Emojis.RIGHT} **Roles**: ${roles} roles`,
+              ].join("\n"),
+          },
+          FIELD_3: {
+            FIELD: "**Creation Date**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+        },
+      },
+      USER: {
+        MESSAGE_1: {
+          TITLE_1: ({ name }: { name: string }) => `**${name} Information**`,
+          FIELD_1: {
+            FIELD: "**General Information**",
+            VALUE: ({ name, id }: { name: string; id: string }) =>
+              [
+                `${Emojis.RIGHT} **User**: ${name}`,
+                `${Emojis.RIGHT} **ID**: ${id}`,
+              ].join("\n"),
+          },
+          FIELD_2: {
+            FIELD: "**Creation Date**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+          FIELD_3: {
+            FIELD: "**Membership Date**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+        },
+      },
+    },
+    UTIL: {
+      SUGGEST: {
+        COMPONENTS: {
+          BUTTONS: {
+            MANAGE: {
+              REPORT: {
+                LABEL: "Report",
+              },
+            },
+            STATUS: {
+              APPROVE: {
+                LABEL: "Approve",
+              },
+              DENY: {
+                LABEL: "Deny",
+              },
+            },
+          },
+        },
+        SUGGESTIONS_NOT_FOUND: `**${Emojis.MARK} The suggestions channel has not been found**`,
+        REVIEW_NOT_FOUND: `**${Emojis.MARK} The review channel has not been found**`,
+        MESSAGE_1: {
+          TITLE_1: ({ username }: { username: string }) =>
+            `**${username} Suggestion**`,
+          FIELD_1: {
+            FIELD: ({ moderator }: { moderator: string }) =>
+              `**Comment by ${moderator}**`,
+          },
+          FIELD_2: {
+            FIELD: ({ moderator }: { moderator: string }) =>
+              `**Suggestion approved by ${moderator}**`,
+          },
+          FIELD_3: {
+            FIELD: ({ moderator }: { moderator: string }) =>
+              `**Suggestion denied by ${moderator}**`,
+          },
+        },
+      },
+    },
+  },
+  HELP: {
+    MESSAGE_1: {
+      TITLE_1: ({ name }: { name: string }) => `**${name} Help Panel**`,
+      FIELD_1: {
+        FIELD: ({ command }: { command: string }) =>
+          `**\`/${command}\` Subcommands**`,
+      },
+    },
+  },
+  GENERAL: {
+    INVALID_GUILD_PROPERTY: ({ structure }: { structure: object }) =>
+      `**${Emojis.MARK} The \`guild\` property is not present in the \`${structure.constructor.name}\` structure**\n${Emojis.RIGHT} Attempts to re-execute the action within a server`,
+    INVALID_GUILD_MEMBER: `**${Emojis.MARK} The user must be a member of the server**`,
+    USER_IS_LIMITED: ({ resets }: { resets: string }) =>
+      `**${Emojis.MARK} You are executing too many actions in too little time**\n${Emojis.RIGHT} You have received a block which will be reset in ${resets}`,
+    PERMISSIONS: {
+      GUILD: {
+        USER: ({ permissions }: { permissions: string }) =>
+          `**${Emojis.MARK} This action could not be performed because you need the ${permissions} permission on the server**`,
+        CLIENT: ({ permissions }: { permissions: string }) =>
+          `**${Emojis.MARK} This action could not be performed because I need the ${permissions} permission on the server**`,
+      },
+      CHANNEL: {
+        USER: ({
+          permissions,
+          channel,
+        }: {
+          permissions: string;
+          channel: string;
+        }) =>
+          `**${Emojis.MARK} This action could not be performed because you need the ${permissions} permission on the channel ${channel}**`,
+        CLIENT: ({
+          permissions,
+          channel,
+        }: {
+          permissions: string;
+          channel: string;
+        }) =>
+          `**${Emojis.MARK} This action could not be performed because I need the ${permissions} permission on the channel ${channel}**`,
+      },
+    },
+    SOMETHING_WENT_WRONG: {
+      COMPONENTS: {
+        BUTTONS: {
+          SUPPORT: {
+            LABEL: "Support Server",
+          },
+        },
+      },
+      MESSAGE_1: ({ name, id }: { name: string; id: string }) =>
+        [
+          `**${Emojis.MARK} The server returned an error of type \`${name}\`**`,
+          `${Emojis.RIGHT} **Report ID**: ${id}`,
+        ].join("\n"),
+    },
+    ONLY_GUILD_OWNER: `**${Emojis.MARK} This action can only be performed by the owner of the server**`,
+    INVALID_USER_COLLECTOR: `**${Emojis.MARK} You cannot run this component**`,
+  },
+};
---
+++ src/locales/Locales/Spanish.ts
@@ -0,0 +1,266 @@
+import colors from "@colors/colors";
+import { type Nullish, codeBlock } from "@sapphire/utilities";
+import { isNullOrUndefined } from "@sapphire/utilities";
+import { Emojis } from "#constants";
+import { padding } from "#util";
+
+export default {
+  COMMANDS: {
+    CONFIG: {
+      LANGUAGE: {
+        MESSAGE_1: `**${Emojis.SUCCESS} El idioma ha sido establecido a \`Español\`**`,
+      },
+      PREMIUM: {
+        CLAIM: {
+          MEMBERSHIP_NOT_FOUND: ({ code }: { code: string }) =>
+            `**${Emojis.MARK} El código \`${code}\` no ha sido encontrado**`,
+          MESSAGE_1: ({ expireDate }: { expireDate: string | Nullish }) =>
+            `**${Emojis.SUCCESS} La membresía premium ha sido reclamada**\n${
+              Emojis.RIGHT
+            } La membresía premium ${
+              isNullOrUndefined(expireDate)
+                ? "`nunca caducará`"
+                : `caducará el \`${expireDate}\``
+            }`,
+        },
+        REVOKE: {
+          COMPONENTS: {
+            BUTTONS: {
+              CONFIRM: {
+                LABEL: "Confirmar",
+                MESSAGE_1: `**${Emojis.SUCCESS} La membresía premium ha sido revocada**`,
+              },
+              CANCEL: {
+                LABEL: "Cancelar",
+                MESSAGE_1: `**${Emojis.SUCCESS} La revocación ha sido cancelada**`,
+              },
+            },
+          },
+          INVALID_GUILD_MEMBERSHIP: `**${Emojis.MARK} El servidor no tiene una membresía premium**`,
+          MESSAGE_1: `**${Emojis.WARNING} Estás a punto de cancelar la membresía premium**\n${Emojis.RIGHT} Si lo haces, ya no podrás disfrutar de los beneficios de la membresía`,
+        },
+      },
+      TIMEZONE: {
+        ERRORS: {
+          TIMEZONE_NOT_FOUND: ({ timezone }: { timezone: string }) =>
+            `**${Emojis.MARK} La zona horaria \`${timezone}\` no ha sido encontrada**`,
+        },
+        MESSAGE_1: ({ timezone }: { timezone: string }) =>
+          `**${Emojis.SUCCESS} La zona horaria ha sido establecida a \`${timezone}\`**`,
+      },
+    },
+    INFO: {
+      BOT: {
+        MESSAGE_1: {
+          TITLE_1: ({ name }: { name: string }) => `**Información de ${name}**`,
+          FIELD_1: {
+            FIELD: "**Información General**",
+            VALUE: ({ version, memory }: { version: string; memory: string }) =>
+              [
+                `${Emojis.RIGHT} **Versión**: ${version}`,
+                `${Emojis.RIGHT} **Uso de RAM**: ${memory}`,
+              ].join("\n"),
+          },
+          FIELD_2: {
+            FIELD: "**Estadísticas**",
+            VALUE: ({
+              users,
+              guilds,
+              shards,
+            }: {
+              users: number;
+              guilds: number;
+              shards: number;
+            }) =>
+              [
+                `${Emojis.RIGHT} **Usuarios**: ${users} usuarios`,
+                `${Emojis.RIGHT} **Servidores**: ${guilds} servidores`,
+                `${Emojis.RIGHT} **Shards**: ${shards} shards`,
+              ].join("\n"),
+          },
+          FIELD_3: {
+            FIELD: "**Fecha del Último Reinicio**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+        },
+      },
+      PING: {
+        MESSAGE_1: {
+          DESCRIPTION_1: ({ rest, shard }: { rest: string; shard: string }) =>
+            codeBlock(
+              "ansi",
+              padding(
+                `${colors.reset.cyan(
+                  "Referencia REST",
+                )} - ${colors.bold.magenta(rest)}\n${colors.reset.cyan(
+                  "Conexión WebSocket",
+                )} - ${colors.bold.magenta(shard)}`,
+                "-",
+              ),
+            ),
+        },
+      },
+      SERVER: {
+        MESSAGE_1: {
+          TITLE_1: ({ name }: { name: string }) => `**Información de ${name}**`,
+          FIELD_1: {
+            FIELD: "**Información General**",
+            VALUE: ({
+              name,
+              id,
+              owner,
+            }: {
+              name: string;
+              id: string;
+              owner: string;
+            }) =>
+              [
+                `${Emojis.RIGHT} **Nombre**: ${name}`,
+                `${Emojis.RIGHT} **ID**: ${id}`,
+                `${Emojis.RIGHT} **Propietario**: ${owner}`,
+              ].join("\n"),
+          },
+          FIELD_2: {
+            FIELD: "**Estadísticas**",
+            VALUE: ({
+              members,
+              channels,
+              roles,
+            }: {
+              members: number;
+              channels: number;
+              roles: number;
+            }) =>
+              [
+                `${Emojis.RIGHT} **Miembros**: ${members} miembros`,
+                `${Emojis.RIGHT} **Canales**: ${channels} canales`,
+                `${Emojis.RIGHT} **Roles**: ${roles} roles`,
+              ].join("\n"),
+          },
+          FIELD_3: {
+            FIELD: "**Fecha de Creación**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+        },
+      },
+      USER: {
+        MESSAGE_1: {
+          TITLE_1: ({ name }: { name: string }) => `**Información de ${name}**`,
+          FIELD_1: {
+            FIELD: "**Información General**",
+            VALUE: ({ name, id }: { name: string; id: string }) =>
+              [
+                `${Emojis.RIGHT} **Usuario**: ${name}`,
+                `${Emojis.RIGHT} **ID**: ${id}`,
+              ].join("\n"),
+          },
+          FIELD_2: {
+            FIELD: "**Fecha de Creación**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+          FIELD_3: {
+            FIELD: "**Fecha de Membresía**",
+            VALUE: ({ date }: { date: string }) => `${Emojis.RIGHT} ${date}`,
+          },
+        },
+      },
+    },
+    UTIL: {
+      SUGGEST: {
+        COMPONENTS: {
+          BUTTONS: {
+            MANAGE: {
+              REPORT: {
+                LABEL: "Reportar",
+              },
+            },
+            STATUS: {
+              APPROVE: {
+                LABEL: "Aprobar",
+              },
+              DENY: {
+                LABEL: "Denegar",
+              },
+            },
+          },
+        },
+        SUGGESTIONS_NOT_FOUND: `**${Emojis.MARK} El canal de sugerencias no ha sido encontrado**`,
+        REVIEW_NOT_FOUND: `**${Emojis.MARK} El canal de revisión no ha sido encontrado**`,
+        MESSAGE_1: {
+          TITLE_1: ({ username }: { username: string }) =>
+            `**Sugerencia de ${username}**`,
+          FIELD_1: {
+            FIELD: ({ moderator }: { moderator: string }) =>
+              `**Comentario de ${moderator}**`,
+          },
+          FIELD_2: {
+            FIELD: ({ moderator }: { moderator: string }) =>
+              `**Sugerencia aprobada por ${moderator}**`,
+          },
+          FIELD_3: {
+            FIELD: ({ moderator }: { moderator: string }) =>
+              `**Sugerencia denegada por ${moderator}**`,
+          },
+        },
+      },
+    },
+  },
+  HELP: {
+    MESSAGE_1: {
+      TITLE_1: ({ name }: { name: string }) => `**Panel de Ayuda de ${name}**`,
+      FIELD_1: {
+        FIELD: ({ command }: { command: string }) =>
+          `**Subcomandos de \`/${command}\`**`,
+      },
+    },
+  },
+  GENERAL: {
+    INVALID_GUILD_PROPERTY: ({ structure }: { structure: object }) =>
+      `**${Emojis.MARK} La propiedad \`guild\` no está presente en la estructura \`${structure.constructor.name}\`**\n${Emojis.RIGHT} Intenta volver a ejecutar la acción dentro de un servidor`,
+    INVALID_GUILD_MEMBER: `**${Emojis.MARK} El usuario debe de ser miembro del servidor**`,
+    USER_IS_LIMITED: ({ resets }: { resets: string }) =>
+      `**${Emojis.MARK} Estas ejecutando demasiadas acciones en poco tiempo**\n${Emojis.RIGHT} Has recibido un bloqueo que se reiniciará en ${resets}`,
+    PERMISSIONS: {
+      GUILD: {
+        USER: ({ permissions }: { permissions: string }) =>
+          `**${Emojis.MARK} No se ha podido realizar esta acción debido a que necesitas el permiso de ${permissions} en el servidor**`,
+        CLIENT: ({ permissions }: { permissions: string }) =>
+          `**${Emojis.MARK} No se ha podido realizar esta acción debido a que necesito el permiso de ${permissions} en el servidor**`,
+      },
+      CHANNEL: {
+        USER: ({
+          permissions,
+          channel,
+        }: {
+          permissions: string;
+          channel: string;
+        }) =>
+          `**${Emojis.MARK} No se ha podido realizar esta acción debido a que necesitas el permiso de ${permissions} en el canal ${channel}**`,
+        CLIENT: ({
+          permissions,
+          channel,
+        }: {
+          permissions: string;
+          channel: string;
+        }) =>
+          `**${Emojis.MARK} No se ha podido realizar esta acción debido a que necesito el permiso de ${permissions} en el canal ${channel}**`,
+      },
+    },
+    SOMETHING_WENT_WRONG: {
+      COMPONENTS: {
+        BUTTONS: {
+          SUPPORT: {
+            LABEL: "Servidor de Soporte",
+          },
+        },
+      },
+      MESSAGE_1: ({ name, id }: { name: string; id: string }) =>
+        [
+          `**${Emojis.MARK} El servidor devolvió un error de tipo \`${name}\`**`,
+          `${Emojis.RIGHT} **ID del Reporte**: ${id}`,
+        ].join("\n"),
+    },
+    ONLY_GUILD_OWNER: `**${Emojis.MARK} Esta acción solamente la puede ejecutar el propietario del servidor**`,
+    INVALID_USER_COLLECTOR: `**${Emojis.MARK} No puedes ejecutar este componente**`,
+  },
+};
