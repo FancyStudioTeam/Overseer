ea525b21ea26ff6e4432a8b7234244201af5b427
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-05-29 17:10:43 +0200

    Update Kick command

--- src/commands/Chat/Moderation/SubCommands/Kick.ts
+++ src/commands/Chat/Moderation/SubCommands/Kick.ts
@@ -1,11 +1,10 @@
-import { cutText } from "@sapphire/utilities";
 import type { CommandInteraction } from "oceanic.js";
 import { BaseBuilder, EmbedBuilder } from "#builders";
 import type { Discord } from "#client";
 import { Colors } from "#constants";
 import { Translations } from "#locales";
 import { type ChatInputSubCommandInterface, Directory } from "#types";
-import { errorMessage, escapeDiscordMarkdown } from "#util";
+import { errorMessage, sanitizeString } from "#util";
 
 export default new BaseBuilder<ChatInputSubCommandInterface>({
   name: "kick",
@@ -30,11 +29,12 @@ export default new BaseBuilder<ChatInputSubCommandInterface>({
     }
 
     const _memberOption = _context.data.options.getMember("user", true);
-    const _reasonOption = cutText(
-      escapeDiscordMarkdown(
-        _context.data.options.getString("reason", true) ?? "No reason",
-      ),
-      35,
+    const _reasonOption = sanitizeString(
+      _context.data.options.getString("reason") ?? "No reason",
+      {
+        maxLength: 35,
+        espaceMarkdown: true,
+      },
     );
 
     if (
@@ -53,20 +53,22 @@ export default new BaseBuilder<ChatInputSubCommandInterface>({
       );
     }
 
-    await _client.rest.guilds
-      .removeMember(_context.guildID, _memberOption.id, _reasonOption)
-      .then(async () => {
-        await _context.reply({
-          embeds: new EmbedBuilder()
-            .setDescription(
-              Translations[locale].COMMANDS.MODERATION.KICK.MESSAGE_1({
-                username: _memberOption.mention,
-                moderator: _context.user.mention,
-              }),
-            )
-            .setColor(Colors.SUCCESS)
-            .toJSONArray(),
-        });
-      });
+    await _client.rest.guilds.removeMember(
+      _context.guildID,
+      _memberOption.id,
+      _reasonOption,
+    );
+
+    await _context.reply({
+      embeds: new EmbedBuilder()
+        .setDescription(
+          Translations[locale].COMMANDS.MODERATION.KICK.MESSAGE_1({
+            username: _memberOption.mention,
+            moderator: _context.user.mention,
+          }),
+        )
+        .setColor(Colors.SUCCESS)
+        .toJSONArray(),
+    });
   },
 });
