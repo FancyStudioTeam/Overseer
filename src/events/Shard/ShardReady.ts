d87be5902f38930b0310bcf5e9a0f1fff709906d
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-06-09 15:19:44 +0200

    Delete presence

--- src/events/Shard/ShardReady.ts
+++ src/events/Shard/ShardReady.ts
@@ -1,20 +1,6 @@
-import { ActivityTypes } from "oceanic.js";
 import { _client } from "#index";
-import { version } from "#package";
 import { LoggerType, logger } from "#util/Util.js";
 
 _client.on("shardReady", (_id: number) => {
-  const shard = _client.shards.get(_id);
-
-  if (shard) {
-    shard.editStatus("online", [
-      {
-        name: "_",
-        state: `🍃 Shard ${shard.id} / 📦 ${version}`,
-        type: ActivityTypes.CUSTOM,
-      },
-    ]);
-  }
-
   logger(LoggerType.INFO, `[Shard ${_id}] Shard has been connected`);
 });
