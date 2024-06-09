de65eaee2e4239171da0e490597799f85d3fc17c
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-06-09 18:59:32 +0200

    BREAKING: Delete `EmbedBuilder`

--- src/builders/Index.ts
+++ src/builders/Index.ts
@@ -2,7 +2,6 @@ import ActionRowBuilder from "./Components/ActionRow";
 import AttachmentBuilder from "./Components/Attachment";
 import BaseBuilder from "./Components/Base";
 import ButtonBuilder from "./Components/Button";
-import EmbedBuilder from "./Components/Embed";
 import ModalBuilder from "./Components/Modal";
 import SelectMenuBuilder from "./Components/SelectMenu";
 import TextInputBuilder from "./Components/TextInput";
@@ -11,7 +10,6 @@ export {
   ActionRowBuilder,
   AttachmentBuilder,
   ButtonBuilder,
-  EmbedBuilder,
   ModalBuilder,
   SelectMenuBuilder,
   TextInputBuilder,
