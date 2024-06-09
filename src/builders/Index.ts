23f73a7684f1570d4e2257a16d7e0b8955e904cb
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-06-09 22:56:21 +0200

    Replace old `AttachmentBuilder`

--- src/builders/Index.ts
+++ src/builders/Index.ts
@@ -1,5 +1,4 @@
-import AttachmentBuilder from "./Components/Attachment";
 import ModalBuilder from "./Components/Modal";
 import SelectMenuBuilder from "./Components/SelectMenu";
 
-export { AttachmentBuilder, ModalBuilder, SelectMenuBuilder };
+export { ModalBuilder, SelectMenuBuilder };
