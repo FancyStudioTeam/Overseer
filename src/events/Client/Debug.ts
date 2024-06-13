909d2b85b83adfe37268ee92260528ea4a1910e9
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-06-13 20:53:43 +0200

    Display objects correctly

--- src/events/Client/Debug.ts
+++ src/events/Client/Debug.ts
@@ -1,7 +1,16 @@
+import { Result } from "@sapphire/result";
 import type { Nullish } from "@sapphire/utilities";
 import { _client } from "#index";
 import { LoggerType, logger } from "#util/Util.js";
 
 _client.on("debug", (_info: string, _shard: number | Nullish) => {
-  logger(LoggerType.DEBUG, `${_shard ? `[Shard ${_shard}]` : "[No Shard]"} ${_info}`);
+  const result = Result.from(() => {
+    const stringified = JSON.stringify(JSON.parse(_info), null, 2);
+
+    logger(LoggerType.DEBUG, `${_shard ? `[Shard ${_shard}]` : "[No Shard]"} ${stringified}`);
+  });
+
+  result.unwrapOrElse(() => {
+    logger(LoggerType.DEBUG, `${_shard ? `[Shard ${_shard}]` : "[No Shard]"} ${_info}`);
+  });
 });
