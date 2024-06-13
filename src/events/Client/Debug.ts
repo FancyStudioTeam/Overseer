2b4c50ecbef3e648747ba8e4a3a92ef3a0018d3a
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-06-13 22:33:43 +0200

    Remove object parsing

--- src/events/Client/Debug.ts
+++ src/events/Client/Debug.ts
@@ -1,16 +1,7 @@
-import { Result } from "@sapphire/result";
 import type { Nullish } from "@sapphire/utilities";
 import { _client } from "#index";
 import { LoggerType, logger } from "#util/Util.js";
 
 _client.on("debug", (_info: string, _shard: number | Nullish) => {
-  const result = Result.from(() => {
-    const stringified = JSON.stringify(JSON.parse(_info), null, 2);
-
-    logger(LoggerType.DEBUG, `${_shard ? `[Shard ${_shard}]` : "[No Shard]"} ${stringified}`);
-  });
-
-  result.unwrapOrElse(() => {
-    logger(LoggerType.DEBUG, `${_shard ? `[Shard ${_shard}]` : "[No Shard]"} ${_info}`);
-  });
+  logger(LoggerType.DEBUG, `${_shard ? `[Shard ${_shard}]` : "[No Shard]"} ${_info}`);
 });
