ba2c9e9c0c23676ceb3be0d3640654b75a22d2a7
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-20 22:03:34 +0200

    Add status management in `ShardReady` event

--- src/events/Shard/ShardReady.ts
+++ src/events/Shard/ShardReady.ts
@@ -1,6 +1,19 @@
+import { ActivityTypes } from "oceanic.js";
 import { _client } from "#index";
 import { LoggerType, logger } from "#util/Util.js";
 
 _client.on("shardReady", (_id: number) => {
+  const shard = _client.shards.get(_id);
+
+  if (shard) {
+    shard.editStatus("online", [
+      {
+        name: ".",
+        state: "🔗 overseerbot.pages.dev",
+        type: ActivityTypes.CUSTOM,
+      },
+    ]);
+  }
+
   logger(LoggerType.INFO, `[Shard ${_id}] Shard has been connected`);
 });
