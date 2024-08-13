6f3ef21fbebc17616dd81589f8cba767bbeccf04
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-13 15:33:34 +0200

    Little change

--- src/classes/Base.ts
+++ src/classes/Base.ts
@@ -1,6 +1,6 @@
 import type { ChatInputCommand, ChatInputSubCommand, Component, Modal, UserCommand } from "#types";
 
-export class Base<T extends BaseTypes = BaseTypes> {
+export class Base<T extends BaseTypes> {
   constructor(options: T) {
     Object.assign(this, options);
   }
