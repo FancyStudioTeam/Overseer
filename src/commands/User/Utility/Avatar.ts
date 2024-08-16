e5e4f628894533edc870cd6acdd217d96f2d08bf
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-16 12:58:22 +0200

    Changed to `createUserCommand`

--- src/commands/User/Utility/Avatar.ts
+++ src/commands/User/Utility/Avatar.ts
@@ -1,14 +1,13 @@
 import { Embed } from "oceanic-builders";
 import { ApplicationCommandTypes, ApplicationIntegrationTypes, InteractionContextTypes, type User } from "oceanic.js";
-import { Base } from "#base";
 import { Colors } from "#constants";
-import type { UserCommand } from "#types";
+import { createUserCommand } from "#util/Handlers";
 
-export default new Base<UserCommand>({
-  name: "Avatar",
-  type: ApplicationCommandTypes.USER,
+export default createUserCommand({
   contexts: [InteractionContextTypes.GUILD],
   integrationTypes: [ApplicationIntegrationTypes.GUILD_INSTALL],
+  name: "Avatar",
+  type: ApplicationCommandTypes.USER,
   run: async ({ context }) => {
     const userOption = context.data.target as User;
 
