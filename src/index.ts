e2035b373dc131c692107c39ea19807776ec5696
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-10-05 15:41:27 +0200

    Use own prototype file

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,4 @@
 import "dotenv/config";
-import { Member, type Role, User } from "oceanic.js";
-import { Discord } from "#client";
+import { Discord } from "@client";
 
 export const client = new Discord();
-
-Object.defineProperty(User.prototype, "name", {
-  get: function () {
-    return this.globalName ?? this.username;
-  },
-});
-
-Object.defineProperty(Member.prototype, "highestRole", {
-  get: function () {
-    const everyoneRole = this.guild.roles.get(this.guildID) as Role;
-    const roles: Role[] = [everyoneRole];
-
-    for (const roleId of this.roles) {
-      const role = this.guild.roles.get(roleId);
-
-      if (!role) continue;
-
-      roles.push(role);
-    }
-
-    return roles.sort((a, b) => b.position - a.position)[0];
-  },
-});
---
+++ src/prototypes.ts
@@ -0,0 +1,24 @@
+import { Member, type Role, User } from "oceanic.js";
+
+Object.defineProperty(User.prototype, "name", {
+  get: function () {
+    return this.globalName ?? this.username;
+  },
+});
+
+Object.defineProperty(Member.prototype, "highestRole", {
+  get: function () {
+    const everyoneRole = this.guild.roles.get(this.guildID) as Role;
+    const roles: Role[] = [everyoneRole];
+
+    for (const roleId of this.roles) {
+      const role = this.guild.roles.get(roleId);
+
+      if (!role) continue;
+
+      roles.push(role);
+    }
+
+    return roles.sort((a, b) => b.position - a.position)[0];
+  },
+});
