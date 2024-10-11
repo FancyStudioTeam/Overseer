d3fa56f537e28ebcd7895750b79a927ceb668ab7
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-10-11 21:27:14 +0200

    Use server emojis instead

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { Development, Production } from "@util/Emojis.js";
-
 export const Colors = {
   COLOR: 0x2b2d31,
   GREEN: /*0x10b981*/ 0x2b2d31,
@@ -9,7 +7,16 @@ export const Colors = {
 
 export const Developers = ["945029082314338407"];
 
-export const Emojis = process.env.NODE_ENV === "development" ? Development : Production;
+export const Emojis = {
+  ARROW_CIRCLE_LEFT: "<:_:1294380124501835846>",
+  ARROW_CIRCLE_RIGHT: "<:_:1294380125567320146>",
+  CANCEL: "<:_:1294380127043584051>",
+  CHECK_CIRCLE: "<:_:1294380128062804019>",
+  EXIT_TO_APP: "<:_:1294380129618759774>",
+  EXPLORE: "<:_:1294380131682615358>",
+  LINK: "<:_:1294380132999630848>",
+  SHIELD_PERSON: "<:_:1294380134228430869>",
+};
 
 export const Links = {
   INVITE: "https://discord.com/oauth2/authorize?client_id=1228065406196125810&permissions=117760&scope=bot",
