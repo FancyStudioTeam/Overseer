dad0de30851b3543e656394632db49bbcc1c4583
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-10-27 17:52:08 +0100

    Fix `createProgressBar` function

--- src/util/utils/createProgressBar.ts
+++ src/util/utils/createProgressBar.ts
@@ -3,9 +3,8 @@ export const createProgressBar = (current: number, total: number, maxTiles = 10)
     fill: "▰",
     empty: "▱",
   };
-  const totalProgress = current + total;
-  const currentProgress = Math.round((current / totalProgress) * maxTiles);
-  const emptyProgress = Math.round((total / totalProgress) * maxTiles);
+  const currentProgress = Math.round((current / total) * maxTiles);
+  const emptyProgress = maxTiles - currentProgress;
 
   return [progressBarSymbols.fill.repeat(currentProgress), progressBarSymbols.empty.repeat(emptyProgress)].join("");
 };
