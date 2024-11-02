93c12c31a92e3507ac2d0e7d3220751b49d174b0
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-11-02 22:47:57 +0100

    Improve debug command

--- src/commands/chatInput/information/commands/Debug.ts
+++ src/commands/chatInput/information/commands/Debug.ts
@@ -1,22 +1,65 @@
 import { Colors } from "@constants";
+import { author as developer, version } from "@package";
 import { Translations } from "@translations";
 import { CommandCategory, createChatInputSubCommand } from "@util/Handlers";
-import { Embed } from "oceanic-builders";
+import { pagination } from "@util/Pagination";
+import { Embed, EmbedField } from "oceanic-builders";
+
+const bytesToMegaBytes = (bytes: number) => bytes / 1000000;
 
 export default createChatInputSubCommand({
   category: CommandCategory.INFORMATION,
   name: "debug",
-  run: async ({ client, context, locale }) =>
-    await context.reply({
-      embeds: new Embed()
+  run: async ({ client, context, locale }) => {
+    const { totalMemory, usedMemory } = {
+      totalMemory: Math.round(bytesToMegaBytes(process.memoryUsage().heapTotal)),
+      usedMemory: Math.round(bytesToMegaBytes(process.memoryUsage().heapUsed)),
+    };
+    const availablePaginationPages = [
+      new Embed()
         .setTitle(Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.TITLE_1)
-        .setDescription(
-          Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.DESCRIPTION_1({
-            guilds: client.guilds.size,
-            users: client.guilds.reduce((previousCount, guild) => previousCount + guild.memberCount, 0),
-          }),
-        )
+        .addFields([
+          new EmbedField().setName(Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.FIELD_1.NAME).setValue(
+            Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.FIELD_1.VALUE({
+              version,
+              developer,
+            }),
+          ),
+          new EmbedField().setName(Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.FIELD_2.NAME).setValue(
+            Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.FIELD_2.VALUE({
+              guildCount: client.guilds.size,
+              shardCount: client.shards.size,
+              userCount: client.guilds.reduce((previousCount, guild) => previousCount + guild.memberCount, 0),
+            }),
+          ),
+          new EmbedField().setName(Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.FIELD_3.NAME).setValue(
+            Translations[locale].COMMANDS.INFORMATION.DEBUG.MESSAGE_1.FIELD_3.VALUE({
+              lastResetDate: client.readyAt,
+            }),
+          ),
+        ])
         .setColor(Colors.COLOR)
-        .toJSON(true),
-    }),
+        .toJSON(),
+      new Embed()
+        .setTitle(Translations[locale].COMMANDS.INFORMATION.DEBUG.COMPONENTS.BUTTONS.PROCESS.MESSAGE_1.TITLE_1)
+        .addFields([
+          new EmbedField()
+            .setName(Translations[locale].COMMANDS.INFORMATION.DEBUG.COMPONENTS.BUTTONS.PROCESS.MESSAGE_1.FIELD_1.NAME)
+            .setValue(
+              Translations[locale].COMMANDS.INFORMATION.DEBUG.COMPONENTS.BUTTONS.PROCESS.MESSAGE_1.FIELD_1.VALUE({
+                totalMemory,
+                usedMemory,
+              }),
+            ),
+        ])
+        .setColor(Colors.COLOR)
+        .toJSON(),
+    ];
+
+    pagination(context, {
+      data: availablePaginationPages,
+      locale,
+      timeBeforeExpiration: 60000,
+    });
+  },
 });
