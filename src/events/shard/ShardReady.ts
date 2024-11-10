d9d1bcb9cbba89e5ab52eedd85a2c9e6f19a415f
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-11-10 20:18:48 +0100

    Stuff

--- src/events/shard/ShardReady.ts
+++ src/events/shard/ShardReady.ts
@@ -1,4 +1,19 @@
 import { client } from "@index";
 import { createLogMessage } from "@utils";
+import { ActivityTypes } from "oceanic.js";
 
-client.on("shardReady", (id) => createLogMessage(`[Shard ${id}] Shard has been connected`));
+client.on("shardReady", (id) => {
+  const shard = client.shards.get(id);
+
+  if (shard) {
+    shard.editStatus("online", [
+      {
+        name: ".",
+        state: "https://overseerdocs.pages.dev/",
+        type: ActivityTypes.CUSTOM,
+      },
+    ]);
+  }
+
+  createLogMessage(`[Shard ${id}] Shard has been connected`);
+});
