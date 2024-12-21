8618043c47669da67ab435dd2343e571429bcb69
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2024-12-21 13:02:51 +0100

    Delete guide

--- apps/docs/i18n/es/docusaurus-plugin-content-docs/current/modules/automations.md
+++
@@ -1,9 +0,0 @@
-# Automatizaciones
-
-Las automatizaciones permiten crear comportamientos del bot personalizados a través de eventos.
-
-## Lenguaje de Scripting
-
-Overseer utiliza un lenguaje de scripting incrustado en YAML llamado [YAMLCord][YAMLCordDocsURL] para crear las automatizaciones.
-
-[YAMLCordDocsURL]: https://yamlcord.pages.dev/
--- src/events/shard/ShardReady.ts
+++ src/events/shard/ShardReady.ts
@@ -1,19 +1,4 @@
 import { client } from "@index";
 import { createLogMessage } from "@utils";
-import { ActivityTypes } from "oceanic.js";
 
-client.on("shardReady", (id) => {
-  const shard = client.shards.get(id);
-
-  if (shard) {
-    shard.editStatus("online", [
-      {
-        name: ".",
-        state: "https://overseerdocs.pages.dev/",
-        type: ActivityTypes.CUSTOM,
-      },
-    ]);
-  }
-
-  createLogMessage(`[Shard ${id}] Shard has been connected`);
-});
+client.on("shardReady", (id) => createLogMessage(`[Shard ${id}] Shard has been connected`));
