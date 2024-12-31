f33f6f200a9113b456d2051cb034fab08dc2e050
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2024-12-31 17:27:13 +0100

    Add client and gateway override

---
+++ apps/client/src/util/Client.ts
@@ -0,0 +1,29 @@
+import { DISCORD_TOKEN, PROXY_AUTHORIZATION, PROXY_URL } from "@config";
+import { Collection, DesiredPropertiesBehavior, createBot } from "@discordeno/bot";
+import { overrideGateway } from "@utils";
+
+const rawClient = createBot({
+  desiredProperties: {
+    user: {
+      username: true,
+    },
+  },
+  desiredPropertiesBehavior: DesiredPropertiesBehavior.ChangeType,
+  rest: {
+    proxy: {
+      authorization: PROXY_AUTHORIZATION,
+      baseUrl: `${PROXY_URL}/rest`,
+    },
+  },
+  token: DISCORD_TOKEN,
+});
+
+export const client = rawClient as Client;
+
+client.commands = new Collection();
+
+overrideGateway(client);
+
+export type Client = typeof rawClient & {
+  commands: Collection<string, unknown>;
+};
---
+++ apps/client/src/util/utils/overrideGateway.ts
@@ -0,0 +1,19 @@
+import { PROXY_AUTHORIZATION, PROXY_URL } from "@config";
+import type { Client } from "../Client.js";
+
+export const overrideGateway = (client: Client) => {
+  client.gateway.sendPayload = async (shardId, payload) => {
+    await fetch(`${PROXY_URL}/gateway`, {
+      body: JSON.stringify({
+        type: "ShardPayload",
+        shardId,
+        payload,
+      }),
+      headers: {
+        authorization: PROXY_AUTHORIZATION,
+        "content-type": "application/json",
+      },
+      method: "POST",
+    });
+  };
+};
