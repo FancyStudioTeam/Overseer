d6d2d546b9a529cf4c7ccee5151b3c535a1a88a3
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-01 00:07:44 +0100

    Add prisma to client object

--- apps/client/src/util/Client.ts
+++ apps/client/src/util/Client.ts
@@ -1,12 +1,18 @@
 import { DISCORD_TOKEN, PROXY_AUTHORIZATION, PROXY_URL } from "@config";
 import { Collection, DesiredPropertiesBehavior, createBot } from "@discordeno/bot";
+import type { PrismaClient } from "@prisma/client";
 import { overrideGateway } from "@utils";
+import { prisma } from "./Prisma.js";
 
 const rawClient = createBot({
   desiredProperties: {
     interaction: {
       id: true,
       token: true,
+    },
+    message: {
+      author: true,
+      content: true,
     },
     user: {
       username: true,
@@ -25,9 +31,11 @@ const rawClient = createBot({
 export const client = rawClient as Client;
 
 client.commands = new Collection();
+client.prisma = prisma;
 
 overrideGateway(client);
 
 export type Client = typeof rawClient & {
   commands: Collection<string, unknown>;
+  prisma: PrismaClient;
 };
