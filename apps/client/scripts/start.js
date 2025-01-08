34e53d5d47ecf756b7df7e10ba960b869675f430
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-08 21:34:40 +0100

    Use `&&`

--- apps/client/scripts/start.js
+++ apps/client/scripts/start.js
@@ -1,5 +1,5 @@
 import { execSync } from "node:child_process";
 
-execSync("SET NODE_ENV=production & npm run prisma:generate & npm run start", {
+execSync("SET NODE_ENV=production && npm run prisma:generate && npm run start", {
   stdio: "inherit",
 });
