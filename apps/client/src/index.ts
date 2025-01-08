4759128e27eeba738ae2ebdb97ddf1d855056653
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-08 21:25:50 +0100

    Move client to index

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,4 +1,62 @@
 import "dotenv/config";
 import "@api";
+import { join } from "node:path";
+import { DISCORD_TOKEN, PROXY_AUTHORIZATION, PROXY_URL } from "@config";
+import { Collection, DesiredPropertiesBehavior, createBot } from "@discordeno/bot";
+import type { CreateUserCommand } from "@types";
+import { getDirnameFromFileUrl, importDirectory, overrideGateway } from "@utils";
 
-export * from "@util/Client.js";
+const rawClient = createBot({
+  desiredProperties: {
+    interaction: {
+      channelId: true,
+      data: true,
+      guildId: true,
+      id: true,
+      token: true,
+      type: true,
+    },
+    message: {
+      author: true,
+      channelId: true,
+      content: true,
+    },
+    user: {
+      avatar: true,
+      discriminator: true,
+      id: true,
+      username: true,
+    },
+  },
+  desiredPropertiesBehavior: DesiredPropertiesBehavior.ChangeType,
+  rest: {
+    proxy: {
+      authorization: PROXY_AUTHORIZATION,
+      baseUrl: `${PROXY_URL}/rest`,
+    },
+  },
+  token: DISCORD_TOKEN,
+});
+
+export const client = rawClient as Client;
+
+client.commands = {
+  chatInput: new Collection(),
+  message: new Collection(),
+  user: new Collection(),
+};
+
+overrideGateway(client);
+
+export type Client = typeof rawClient & {
+  commands: {
+    chatInput: Collection<string, unknown>;
+    message: Collection<string, unknown>;
+    user: Collection<string, CreateUserCommand>;
+  };
+};
+
+const currentFolder = getDirnameFromFileUrl(import.meta.url);
+
+await importDirectory(join(currentFolder, "events"));
+await importDirectory(join(currentFolder, "commands"));
