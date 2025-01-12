e6f09acd1b4dc179f7042c2e72163a823d2feb90
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-13 00:08:57 +0100

    Delete `createReplyOrMessage` in favor of `createMessage`

--- apps/client/src/util/utils/createMessage.ts
+++ apps/client/src/util/utils/createMessage.ts
@@ -1,22 +1,69 @@
+import { InteractionResponseTypes } from "@discordeno/bot";
+import { client } from "@index";
 import type { AnyContext, AnyMessagePayload, Message } from "@types";
-import { createReplyOrMessage } from "./createReplyOrMessage.js";
 import { resolveMessagePayload } from "./resolveMessagePayload.js";
 
-export const createMessage = async (
-  context: AnyContext,
+export const createMessage = async <Context extends AnyContext, WithMessage extends boolean>(
+  context: Context,
   content: AnyMessagePayload,
-  options: CreateMessageOptions = {
-    isEphemeral: false,
-  },
-): Promise<Message> => {
-  const { isEphemeral } = options;
-  const messagePayload = resolveMessagePayload(content, {
-    isEphemeral,
-  });
+  options: CreateMessageOptions<WithMessage> = {
+    withMessage: false,
+  } as CreateMessageOptions<WithMessage>,
+): Promise<CreateMessage<Context, WithMessage>> => {
+  const messagePayload = resolveMessagePayload(content);
+  /**
+   * The "withMessage" option is only used when the provided context is an interaction.
+   * Methods like "sendFollowupMessage" and "sendMessage" always return a message object.
+   */
+  const { withMessage } = options;
 
-  return await createReplyOrMessage(context, messagePayload);
+  if ("acknowledged" in context) {
+    const { acknowledged, id, token } = context;
+
+    if (acknowledged) {
+      return await client.helpers.sendFollowupMessage(token, messagePayload);
+    }
+
+    await client.helpers.sendInteractionResponse(id, token, {
+      type: InteractionResponseTypes.ChannelMessageWithSource,
+      data: messagePayload,
+    });
+
+    if (withMessage) {
+      return await client.helpers.getOriginalInteractionResponse(token);
+    }
+
+    return undefined as CreateMessage<Context, WithMessage>;
+  }
+
+  const { channelId } = context;
+
+  return await client.helpers.sendMessage(channelId, messagePayload);
 };
 
-interface CreateMessageOptions {
-  isEphemeral?: boolean;
+interface CreateMessageOptions<WithMessage extends boolean> {
+  /**
+   * Fetchs and returns the original message object from the interaction response.
+   */
+  withMessage?: WithMessage;
 }
+
+/**
+ * A conditional type that determinates the return type based on the "WithMessage" generic.
+ *
+ * By default, return type will be "Message | undefined".
+ * If "WithMessage" is "true", return type will be "Message".
+ */
+type CreateMessageUsingInteraction<WithMessage extends boolean> = WithMessage extends true
+  ? Message
+  : Message | undefined;
+
+/**
+ * A conditional type that determinates the return type based on the context.
+ *
+ * When using messages, return type will be "Message".
+ * When using interactions, return type will be "Message | undefined".
+ */
+type CreateMessage<Context extends AnyContext, WithMessage extends boolean> = Context extends Message
+  ? Message
+  : CreateMessageUsingInteraction<WithMessage>;
