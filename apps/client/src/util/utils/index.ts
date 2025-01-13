d0244ba8ebcb16fbf9bd361f92571e563e6e6328
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-13 18:42:01 +0100

    Move from `util/utils` to `functions/*`

--- apps/client/src/util/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./createMessage.js";
-export * from "./env.js";
-export * from "./getDirnameFromFileUrl.js";
-export * from "./loadDirectoryFiles.js";
-export * from "./overrideGateway.js";
-export * from "./resolveMessagePayload.js";
