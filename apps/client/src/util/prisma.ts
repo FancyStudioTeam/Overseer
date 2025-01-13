983f158cd852a8b81fad9fc4da24113d5da6a4a1
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-13 18:50:41 +0100

    Delete prisma event listener

--- apps/client/src/util/prisma.ts
+++ apps/client/src/util/prisma.ts
@@ -1,15 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import { logger } from "./logger.js";
 
-export const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-  ],
-});
-
-prisma.$on("query", ({ duration, params, query }) =>
-  logger.info(`Sent Prisma query to MongoDB: "${query}" with params "${params}". Took ${duration} milliseconds.`),
-);
+export const prisma = new PrismaClient();
