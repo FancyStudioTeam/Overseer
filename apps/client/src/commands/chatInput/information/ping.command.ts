4ac74cb50928aae7bfb3507171ca5baf0e34eb1b
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-17 22:45:46 +0100

    Update ping command

--- apps/client/src/commands/chatInput/information/ping.command.ts
+++ apps/client/src/commands/chatInput/information/ping.command.ts
@@ -9,6 +9,7 @@ import {
 } from "@discordeno/bot";
 import { codeBlock } from "@discordjs/formatters";
 import { createMessage } from "@functions/client/createMessage.js";
+import type { MaybeAwaitable } from "@types";
 import { ChatInputSubCommand, type ChatInputSubCommandRunOptions } from "@util/handlers.js";
 import { prisma } from "@util/prisma.js";
 
@@ -22,10 +23,10 @@ export default class PingCommand extends ChatInputSubCommand {
   }
 
   async run({ client, context, t }: ChatInputSubCommandRunOptions): Promise<void> {
-    const [apiRequestsLatency, databaseLatency] = await Promise.all([
-      this.getApiRequestsLatency(client.rest),
-      this.getDatabaseLatency(context.guildId),
-    ]);
+    const { apiRequests: apiRequestsLatency, database: databaseLatency } = await this.getLatencies(
+      client.rest,
+      context.guildId,
+    );
     const [apiRequestsLatencyField, databaseLatencyField]: EmbedField[] = [
       {
         inline: true,
@@ -44,43 +45,40 @@ export default class PingCommand extends ChatInputSubCommand {
     });
   }
 
-  /**
-   * Calculates the APi requests latency.
-   * @param rest The REST manager.
-   * @returns The API requests latency in milliseconds.
-   */
-  async getApiRequestsLatency(rest: RestManager): Promise<number> {
-    const startHrtime = process.hrtime.bigint();
+  async getLatencies(rest: RestManager, guildIdBigString?: BigString): Promise<GetLatenciesData> {
+    const apiRequestPromise = () => rest.getCurrentUser(DISCORD_TOKEN);
+    const databasePromise = () =>
+      prisma.guildConfiguration.findUnique({
+        select: {
+          guildId: true,
+        },
+        where: {
+          guildId: guildIdBigString?.toString(),
+        },
+      });
+    const [apiRequestsLatency, databaseLatency] = await Promise.all([
+      this.getExecutionTime(apiRequestPromise),
+      this.getExecutionTime(databasePromise),
+    ]);
 
-    /**
-     * Sends a request to fetch the current user and waits until the promise is resolved or rejected.
-     */
-    await rest.getCurrentUser(DISCORD_TOKEN);
-
-    const endHrtime = process.hrtime.bigint();
-    const executionTime = this.calculateExecutionTime(startHrtime, endHrtime);
-    const roundedExecutionTime = Math.round(executionTime);
-
-    return roundedExecutionTime;
+    return {
+      apiRequests: apiRequestsLatency,
+      database: databaseLatency,
+    };
   }
 
   /**
-   * Calculates the database latency.
-   * @param guildIdBigString The guild ID as a BigString.
-   * @returns The database latency in milliseconds.
+   * Gets the execution time of a promise.
+   * @param promise The promise to execute.
+   * @returns The promise execution time in milliseconds.
    */
-  async getDatabaseLatency(guildIdBigString?: BigString): Promise<number> {
-    const guildId = guildIdBigString?.toString();
+  async getExecutionTime(promise: (...args: unknown[]) => MaybeAwaitable<unknown>): Promise<number> {
     const startHrtime = process.hrtime.bigint();
 
     /**
-     * Sends a query to the database and waits until the promise is resolved or rejected.
+     * Executes the promise and waits until the promise is resolved or rejected.
      */
-    await prisma.guildConfiguration.findUnique({
-      where: {
-        guildId,
-      },
-    });
+    await promise();
 
     const endHrtime = process.hrtime.bigint();
     const executionTime = this.calculateExecutionTime(startHrtime, endHrtime);
@@ -97,8 +95,20 @@ export default class PingCommand extends ChatInputSubCommand {
    */
   calculateExecutionTime(startTime: bigint, endTime: bigint): number {
     const nanoseconds = Number(endTime - startTime);
+    /*  */
     const milliseconds = nanoseconds / 1e6; /* 1_000_000 */
 
     return milliseconds;
   }
+}
+
+interface GetLatenciesData {
+  /**
+   * The API requests latency in milliseconds.
+   */
+  apiRequests: number;
+  /**
+   * The database latency in milliseconds.
+   */
+  database: number;
 }
