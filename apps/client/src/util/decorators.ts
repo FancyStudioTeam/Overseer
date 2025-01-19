3b86b5f1ea2ed98f2490fe4fbbf5c60b08a3d009
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-19 23:11:24 +0100

    Update `decorators.ts`

--- apps/client/src/util/decorators.ts
+++ apps/client/src/util/decorators.ts
@@ -1,14 +1,17 @@
-import type { ChatInputCommand } from "./handlers.js";
+import type { ChatInputCommand } from "@structures/commands/ChatInputCommand.js";
 
 /**
  * Sets "_autoLoad" property to true in the target class.
  * @returns The updated target class.
  */
-export const AutoLoad = () => {
-  return (target: {
+export const AutoLoad =
+  () =>
+  (target: {
     new (...args: unknown[]): ChatInputCommand;
   }) =>
+    /**
+     * Returns the updated target class.
+     */
     class extends target {
       _autoLoad = true;
     };
-};
