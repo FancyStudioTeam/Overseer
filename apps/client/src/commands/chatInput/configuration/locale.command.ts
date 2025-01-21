f7d3318f5df24b48d4d8fa1df9b237ecbbd35e61
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-21 22:53:46 +0100

    Add locale command

---
+++ apps/client/src/commands/chatInput/configuration/locale.command.ts
@@ -0,0 +1,79 @@
+import { ApplicationCommandOptionTypes } from "@discordeno/bot";
+import { createMessage } from "@functions/createMessage.js";
+import type { GuildPreferencesLocale } from "@prisma/client";
+import { ChatInputSubCommand, type ChatInputSubCommandRunOptions } from "@structures/commands/ChatInputSubCommand.js";
+import { prisma } from "@util/prisma.js";
+
+export default class LocaleCommand extends ChatInputSubCommand {
+  constructor() {
+    super({
+      description: "Updates the bot locale.",
+      name: "locale",
+      options: [
+        {
+          choices: [
+            {
+              name: "English",
+              value: "EN",
+            },
+            {
+              name: "Spanish",
+              value: "ES",
+            },
+          ],
+          description: "_",
+          name: "locale",
+          required: true,
+          type: ApplicationCommandOptionTypes.String,
+        },
+      ],
+      type: ApplicationCommandOptionTypes.SubCommand,
+    });
+  }
+
+  async run({ context, t, options }: ChatInputSubCommandRunOptions<LocaleOptions>): Promise<void> {
+    if (!context.guildId) {
+      return;
+    }
+
+    const { locale: localeOptions } = options;
+    const { locale } = localeOptions;
+    const guildIdBigInt = context.guildId;
+    const guildId = guildIdBigInt.toString();
+    /**
+     * Update the guild preferences locale.
+     * Using the "upsert" method will avoid manual updates for existing or missing guilds.
+     */
+    const { locale: updatedLocale } = await prisma.guildPreferences.upsert({
+      create: {
+        guildId,
+        locale,
+      },
+      select: {
+        locale: true,
+      },
+      update: {
+        locale,
+      },
+      where: {
+        guildId,
+      },
+    });
+
+    await createMessage(
+      context,
+      t("categories.configuration.locale.message_1", {
+        /**
+         * Use the updated locale to translate the message.
+         */
+        lng: updatedLocale.toLowerCase(),
+      }),
+    );
+  }
+}
+
+interface LocaleOptions {
+  locale: {
+    locale: GuildPreferencesLocale;
+  };
+}
