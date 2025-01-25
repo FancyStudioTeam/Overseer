02dc7ffa9f8f8a9965daa638e4afa6c63a52dc56
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-25 17:34:45 +0100

    chore(i18n): add new `common` namespace

--- apps/client/src/util/i18n.ts
+++ apps/client/src/util/i18n.ts
@@ -1,19 +1,23 @@
 import i18next from "i18next";
 import enCommands from "../locales/en/commands.json" with { type: "json" };
+import enCommon from "../locales/en/common.json" with { type: "json" };
 import esCommands from "../locales/es/commands.json" with { type: "json" };
+import esCommon from "../locales/es/common.json" with { type: "json" };
 
 export const i18n = i18next.createInstance();
 
 i18n.init({
   defaultNS: "commands",
   fallbackLng: "en",
-  ns: ["commands"],
+  ns: ["commands", "common"],
   resources: {
     en: {
       commands: enCommands,
+      common: enCommon,
     },
     es: {
       commands: esCommands,
+      common: esCommon,
     },
   },
 });
@@ -23,8 +27,10 @@ declare module "i18next" {
     defaultNS: "commands";
     resources: {
       commands: Commands;
+      common: Common;
     };
   }
 }
 
 type Commands = typeof enCommands | typeof esCommands;
+type Common = typeof enCommon | typeof esCommon;
