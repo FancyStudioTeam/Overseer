e9d5968b2fa565061eaba0655d47b0a7cb93280a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-26 14:51:22 +0100

    chore(permissions): manage command permissions in `interactionCreate` event

--- apps/client/src/events/client/guild/interactionCreate.ts
+++ apps/client/src/events/client/guild/interactionCreate.ts
@@ -4,27 +4,57 @@ import {
   InteractionTypes,
   commandOptionsParser,
 } from "@discordeno/bot";
+import { inlineCode } from "@discordjs/formatters";
+import { createMessage } from "@functions/createMessage.js";
 import { client } from "@index";
+import type { ChatInputSubCommand } from "@structures/commands/ChatInputSubCommand.js";
 import type { Interaction, MaybeOptional, Member, User } from "@types";
+import type { CommandOptionsPermissions } from "@util/decorators.js";
 import { i18n } from "@util/i18n.js";
 import { prisma } from "@util/prisma.js";
 import { match } from "ts-pattern";
 
 /**
- * Gets the sub command names from chat input commands.
+ * Get the required command permissions from the command instance.
+ * @param command The command instance.
+ * @returns The command permissions object.
+ */
+const getCommandPermissions = (command: ChatInputSubCommand): Partial<CommandOptionsPermissions> => {
+  const { _commandOptions: options } = command;
+  const { permissions } = options;
+
+  /** If the command options do not have any permissions, return an empty object. */
+  if (!permissions) {
+    return {};
+  }
+
+  /**
+   * If the permissions are a string, return an object with the user permission.
+   * Otherwise, return the permissions object.
+   */
+  return typeof permissions === "string"
+    ? {
+        user: permissions,
+      }
+    : permissions;
+};
+
+/**
+ * Get the sub command names from chat input commands.
  * @param interaction The interaction context.
  * @returns The sub command names.
  */
 const getSubCommands = (interaction: Interaction): string[] => {
   const { data } = interaction;
   const { options } = data ?? {};
+  /** Find the first sub command option from the interaction data. */
   const subCommand = options?.find((option) => option.type === ApplicationCommandOptionTypes.SubCommand);
 
   return subCommand ? [subCommand.name] : [];
 };
 
 /**
- * Gets the parsed command options object.
+ * Get the parsed command options object.
  * @param interaction The interaction context.
  * @returns The parsed command options.
  */
@@ -36,7 +66,7 @@ const parsedCommandOptions = (interaction: Interaction) => {
 };
 
 /**
- * Gets the target member from user context commands, if any.
+ * Get the target member from user context commands, if any.
  * @param interaction The interaction context.
  * @returns The target member.
  */
@@ -44,16 +74,14 @@ const getTargetMember = (interaction: Interaction): MaybeOptional<Member> => {
   const { data } = interaction;
   const { resolved } = data ?? {};
   const resolvedMembersCollection = resolved?.members;
-  /**
-   * Target members are always in the first resolved members collection position.
-   */
+  /** Target members are in the first position in the resolved members collection. */
   const targetMember = resolvedMembersCollection?.first();
 
   return targetMember;
 };
 
 /**
- * Gets the target user from user context commands.
+ * Get the target user from user context commands.
  * @param interaction The interaction context.
  * @returns The target user.
  */
@@ -61,14 +89,10 @@ const getTargetUser = (interaction: Interaction): User => {
   const { data } = interaction;
   const { resolved } = data ?? {};
   const resolvedUsersCollection = resolved?.users;
-  /**
-   * Target users are always in the first resolved users collection position.
-   */
+  /** Target users are in the first position in the resolved users collection. */
   const targetUser = resolvedUsersCollection?.first();
 
-  /**
-   * User context commands must always include a target user.
-   */
+  /** Throw an error if the target user was not found. */
   if (!targetUser) {
     throw new Error("Cannot get target user from interaction.");
   }
@@ -77,12 +101,13 @@ const getTargetUser = (interaction: Interaction): User => {
 };
 
 client.events.interactionCreate = async (interaction) => {
-  if (!interaction.guildId) {
+  if (!(interaction.guildId && interaction.member)) {
     return;
   }
 
-  const { guildId: guildIdBigInt } = interaction;
+  const { guildId: guildIdBigInt, member } = interaction;
   const guildId = guildIdBigInt.toString();
+  const clientMember = await client.fetchMember(guildIdBigInt);
   const guildConfiguration = await prisma.guildPreferences.findUnique({
     where: {
       guildId,
@@ -91,7 +116,7 @@ client.events.interactionCreate = async (interaction) => {
   const { locale } = {
     locale: (guildConfiguration?.locale ?? "EN").toLowerCase(),
   };
-  const t = i18n.getFixedT(locale, "commands");
+  const tCommands = i18n.getFixedT(locale, "commands");
 
   match(interaction).with(
     {
@@ -112,11 +137,28 @@ client.events.interactionCreate = async (interaction) => {
           const command = client.applicationCommands.chatInput.get([commandName, subCommandNames].join("_"));
 
           if (command) {
+            const permissions = getCommandPermissions(command);
+
+            if (permissions) {
+              const { user: userPermission } = permissions;
+              const tCommon = i18n.getFixedT(locale, "common");
+
+              if (userPermission && !member.permissions?.has(userPermission)) {
+                return createMessage(
+                  applicationCommandInteraction,
+                  tCommon("missing_user_command_permissions", {
+                    permissions: inlineCode(tCommon(`permission_keys.${userPermission}`)),
+                  }),
+                );
+              }
+            }
+
             await command.run({
               client,
+              clientMember,
               context: applicationCommandInteraction,
               options: parsedCommandOptions(applicationCommandInteraction),
-              t,
+              t: tCommands,
             });
           }
         })
@@ -128,8 +170,9 @@ client.events.interactionCreate = async (interaction) => {
           if (command) {
             await command.run({
               client,
+              clientMember,
               context: applicationCommandInteraction,
-              t,
+              t: tCommands,
               targetMember,
               targetUser,
             });
