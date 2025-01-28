805862cb18b52bec818a807549161d2d06e9cd35
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-28 22:53:00 +0100

    chore: add `try/catch` to handler

--- apps/client/src/proxy/app/app.controller.ts
+++ apps/client/src/proxy/app/app.controller.ts
@@ -1,5 +1,14 @@
 import type { DiscordGatewayPayload, GatewayDispatchEventNames } from "@discordeno/bot";
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, UsePipes } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  InternalServerErrorException,
+  Post,
+  UsePipes,
+} from "@nestjs/common";
 import { ZodValidationPipe } from "@proxy/pipes/zodValidation.pipe.js";
 import { EventSchema, type EventSchemaType } from "@proxy/schemas/EventSchema.js";
 import { client } from "@util/client.js";
@@ -17,17 +26,21 @@ export class AppController {
   @UsePipes(new ZodValidationPipe(EventSchema))
   @HttpCode(HttpStatus.NO_CONTENT)
   async handleEvent(@Body() event: EventSchemaType) {
-    const { payload: rawPayload, shardId } = event;
-    const payload = rawPayload as unknown as DiscordGatewayPayload;
-    const { t } = payload;
+    try {
+      const { payload: rawPayload, shardId } = event;
+      const payload = rawPayload as unknown as DiscordGatewayPayload;
+      const { t } = payload;
 
-    client.events.raw?.(payload, shardId);
+      client.events.raw?.(payload, shardId);
 
-    if (t) {
-      const dispatchEventName = t as GatewayDispatchEventNames;
+      if (t) {
+        const dispatchEventName = t as GatewayDispatchEventNames;
 
-      await client.events.dispatchRequirements?.(payload, shardId);
-      client.handlers[dispatchEventName]?.(client, payload, shardId);
+        await client.events.dispatchRequirements?.(payload, shardId);
+        client.handlers[dispatchEventName]?.(client, payload, shardId);
+      }
+    } catch (internalError) {
+      throw new InternalServerErrorException(internalError);
     }
   }
 }
