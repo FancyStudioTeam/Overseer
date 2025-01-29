6dee349b22c7d3e5923032cecc942f354e63acdf
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-01-29 22:33:24 +0100

    fix(nest): fix `Root plugin has already booted`

--- apps/client/src/proxy/index.ts
+++ apps/client/src/proxy/index.ts
@@ -11,18 +11,20 @@ import { AuthGuard } from "./guards/auth.guards.js";
 /** Create a fastify adapter for Nest. */
 const fastifyAdapter = new FastifyAdapter();
 const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter, {
+  cors: true,
   /** Disable the default Nest logger. */
+  logger: false,
 });
+
+app.register(fastifyHelmet);
+
+// biome-ignore lint/correctness/useHookAtTopLevel: Not a React hook.
+app.useGlobalGuards(new AuthGuard());
+// biome-ignore lint/correctness/useHookAtTopLevel: Not a React hook.
+app.useGlobalFilters(new HttpExceptionFilter());
 
 await app.listen(EVENTS_PORT, EVENTS_HOST, async () => {
   const address = await app.getUrl();
 
   logger.http(`Initialized proxy server at address "${address}".`);
 });
-
-await app.register(fastifyHelmet);
-
-// biome-ignore lint/correctness/useHookAtTopLevel: Not a React hook.
-app.useGlobalGuards(new AuthGuard());
-// biome-ignore lint/correctness/useHookAtTopLevel: Not a React hook.
-app.useGlobalFilters(new HttpExceptionFilter());
