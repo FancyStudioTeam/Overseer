a4c678defbd259868df9cce4be974350fc7ffe06
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-02-02 00:51:09 +0100

    refactor: update gateway management

--- apps/proxy/src/managers/GatewayManager.ts
+++ apps/proxy/src/managers/GatewayManager.ts
@@ -1,16 +1,7 @@
-import { type GatewayOpcodes, createGatewayManager } from "@discordeno/bot";
-import {
-  DISCORD_TOKEN,
-  EVENTS_AUTHORIZATION,
-  EVENTS_URL,
-  GATEWAY_INTENTS,
-  SHARDS_PER_WORKER,
-  TOTAL_SHARDS,
-  TOTAL_WORKERS,
-} from "@util/config.js";
-import { OPCODES_NAMES, SENDABLE_OPCODES } from "@util/constants.js";
-import { logger } from "@util/logger.js";
+import { createGatewayManager } from "@discordeno/bot";
+import { DISCORD_TOKEN, GATEWAY_INTENTS, SHARDS_PER_WORKER, TOTAL_SHARDS, TOTAL_WORKERS } from "@util/config.js";
 import { RestManager } from "./RestManager.js";
+import { overrideGatewayManager } from "./functions/overrideGatewayManager.js";
 
 const gatewayConnection = RestManager.getGatewayBot();
 
@@ -32,31 +23,4 @@ export const GatewayManager = createGatewayManager({
   totalWorkers: Number.parseInt(TOTAL_WORKERS),
 });
 
-GatewayManager.events.message = async ({ id }, payload) => {
-  const { op: rawOp } = payload;
-  const op = rawOp as GatewayOpcodes;
-
-  logger.info(`Received "${OPCODES_NAMES[op]}" payload from shard ${id}.`);
-
-  if (!SENDABLE_OPCODES.includes(op)) {
-    return;
-  }
-
-  await fetch(`${EVENTS_URL}/events`, {
-    body: JSON.stringify({
-      payload,
-      shardId: id,
-    }),
-    headers: {
-      authorization: EVENTS_AUTHORIZATION,
-      "content-type": "application/json",
-    },
-    method: "POST",
-  })
-    .then(() => logger.info(`Sent "${OPCODES_NAMES[op]}" payload to events handler.`))
-    .catch((fetchError) => {
-      const error = fetchError instanceof Error ? fetchError.stack : new Error(fetchError);
-
-      logger.error(error);
-    });
-};
+overrideGatewayManager(GatewayManager);
