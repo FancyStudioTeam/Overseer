da6a179303138704a70752d2e52f9276a3c3128a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-02-03 23:43:14 +0100

    docs: add `TODO` comment

--- apps/proxy/src/proxy/modules/gateway/gateway.controller.ts
+++ apps/proxy/src/proxy/modules/gateway/gateway.controller.ts
@@ -2,6 +2,7 @@ import { GatewayManager } from "@managers/GatewayManager.js";
 import { Controller } from "@nestjs/common";
 
 @Controller("gateway")
+/** TODO: Add shard payloads handler. */
 export class GatewayController {}
 
 await GatewayManager.spawnShards();
