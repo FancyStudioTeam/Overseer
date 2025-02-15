9332661531643732400d59df38d63a1f81af9e62
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-02-15 17:43:18 +0100

    chore: update events

--- apps/client/src/events/process/uncaughtException.event.ts
+++ apps/client/src/events/process/uncaughtException.event.ts
@@ -1,3 +1,3 @@
 import { logger } from "@util/logger.js";
 
-process.on("uncaughtException", (error) => logger.error(error.stack));
+process.on("uncaughtException", ({ stack }) => logger.error("Uncaught Exception: ", stack));
--- apps/client/src/events/process/unhandledRejection.event.ts
+++ apps/client/src/events/process/unhandledRejection.event.ts
@@ -1,10 +1,8 @@
+import { getErrorInstance } from "@functions/getErrorInstance.js";
 import { logger } from "@util/logger.js";
 
 process.on("unhandledRejection", (reason) => {
-  /**
-   * Reason is unknown, so transform it to an error instance and log its stack.
-   */
-  const error = reason instanceof Error ? reason : new Error(String(reason));
+  const { stack } = getErrorInstance(reason);
 
-  logger.error(error.stack);
+  logger.error("Unhandled Rejection: ", stack);
 });
