69ac1a13f8dc2a7c506292d1caa88fe55f24f6bd
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-02-22 21:42:55 +0100

    fix: some routes don't require a body

--- apps/proxy/src/proxy/modules/rest/schemas/RequestSchema.ts
+++ apps/proxy/src/proxy/modules/rest/schemas/RequestSchema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
-export const RequestSchema = z.object({}).passthrough();
+export const RequestSchema = z.object({}).passthrough().optional();
 
 export type RequestSchemaDto = z.infer<typeof RequestSchema>;
