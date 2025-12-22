ed951aa07b79c991da2da35cadb04f17d034d0ab
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-22 02:35:57 +0100

    chore: suppress warning from Biome

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
 	apps: [
 		{
 			env: {
+				/*
+				 * biome-ignore lint/style/useNamingConvention: This environment variable
+				 * is used internally in Linkcord to resolve the root directory.
+				 */
 				NODE_ENV: "production",
 			},
 			interpreter: "node",
