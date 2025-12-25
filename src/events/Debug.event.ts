23837d34aa8d68fd6cb19a9de2d70eb699509106
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-25 18:00:21 +0100

    chore: remove old src files

--- src/events/Debug.event.ts
+++
@@ -1,9 +0,0 @@
-import { ClientEvents, defineEventConfig, type EventHandler } from "linkcord";
-
-export const config = defineEventConfig({
-	name: ClientEvents.Debug,
-});
-
-export const handler: EventHandler<ClientEvents.Debug> = ({ message }) => {
-	console.log(message);
-};
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { loadEnvFile } from "node:process";
-import { Client } from "linkcord";
-
-loadEnvFile();
-
-export const client = new Client();
-
-client.init().catch(console.error);
