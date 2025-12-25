06bd587771fd5a1bdbf013b93b8ae1a4257702dc
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-25 23:11:51 +0100

    feat: basic session and logout routes

---
+++ apps/dashboard/server/api/auth/logout.get.ts
@@ -0,0 +1,11 @@
+export default defineEventHandler(async (event) => {
+	try {
+		const { clear } = await sessionManager(event);
+
+		await clear();
+	} catch (error) {
+		console.error(error);
+	} finally {
+		sendRedirect(event, "/");
+	}
+});
---
+++ apps/dashboard/server/api/auth/session.get.ts
@@ -0,0 +1,39 @@
+const INTERNAL_SERVER_ERROR_STATUS = 500;
+const UNAUTHORIZED_STATUS = 401;
+
+export default defineEventHandler(async (event) => {
+	try {
+		const { data, isAuthorized } = await sessionManager(event);
+
+		if (!isAuthorized) {
+			setResponseStatus(event, UNAUTHORIZED_STATUS, "Unauthorized");
+
+			return {
+				is_authorized: false,
+				success: false,
+			};
+		}
+
+		/*
+		 * Clone the "data" object to avoid mutating the original object.
+		 */
+		const clonedData = structuredClone(data);
+
+		/*
+		 * Do not expose "session_id" to the client.
+		 */
+		Reflect.deleteProperty(clonedData, "session_id");
+
+		return {
+			data: clonedData,
+			is_authorized: true,
+			success: true,
+		};
+	} catch {
+		setResponseStatus(event, INTERNAL_SERVER_ERROR_STATUS, "Internal Server Error");
+
+		return {
+			success: false,
+		};
+	}
+});
