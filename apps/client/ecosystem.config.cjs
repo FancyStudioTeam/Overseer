07ad7ca5da740bc9b30be0b00baf52aa413b3eff
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-25 18:22:24 +0100

    chore(client): add `client` application

---
+++ apps/client/ecosystem.config.cjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+/**
+ * @type {EcosystemConfig}
+ */
+module.exports = {
+	apps: [
+		{
+			env: {
+				/*
+				 * biome-ignore lint/style/useNamingConvention: This environment variable
+				 * is used internally in Linkcord to resolve the root directory.
+				 */
+				NODE_ENV: "production",
+			},
+			interpreter: "node",
+			name: "Discord Client",
+			script: "./dist/index.js",
+		},
+	],
+};
+
+/**
+ * @typedef {Object} EcosystemConfig
+ * @property {import("pm2").StartOptions[]} apps
+ */
---
+++ apps/client/src/events/MessageCreate.event.ts
@@ -0,0 +1,39 @@
+import { ClientEvents, ComponentType, defineEventConfig, type EventHandler, MessageFlags } from "linkcord";
+import { bold } from "linkcord/utils";
+import { client } from "../index.js";
+
+export const config = defineEventConfig({
+	name: ClientEvents.MessageCreate,
+});
+
+export const handler: EventHandler<ClientEvents.MessageCreate> = async ({ message }) => {
+	const { rest } = client;
+	const {
+		api: { channels },
+	} = rest;
+	const { channelId, content } = message;
+
+	if (content === ">ping") {
+		const { gateway } = client;
+		const { averageLatency } = gateway;
+
+		await channels.createMessage(channelId, {
+			components: [
+				{
+					components: [
+						{
+							content: [
+								bold(`Average Gateway Ping: ${averageLatency}`),
+							].join("\n"),
+							type: ComponentType.TextDisplay,
+						},
+					],
+					type: ComponentType.Container,
+				},
+			],
+			flags: [
+				MessageFlags.IsComponentsV2,
+			],
+		});
+	}
+};
