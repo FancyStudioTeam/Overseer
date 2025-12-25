164eadd7e3b06610c7efeb3ef47224cc298b47f1
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-25 22:11:21 +0100

    feat(types): add `H3SessionInfo`

--- apps/dashboard/app/composables/useAuth.ts
+++ apps/dashboard/app/composables/useAuth.ts
@@ -1,7 +1,9 @@
 export function useAuth() {
 	const login = () => navigateTo("/api/auth/login");
+	const logout = () => navigateTo("/api/auth/logout");
 
 	return {
 		login,
+		logout,
 	};
 }
---
+++ apps/dashboard/shared/types/session.ts
@@ -0,0 +1,5 @@
+export interface H3SessionInfo {
+	avatar_hash: string;
+	session_id: string;
+	username: string;
+}
