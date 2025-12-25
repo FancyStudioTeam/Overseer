d8ac384af3c4eab243bd7d1765b0174ad272286f
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-25 21:10:37 +0100

    feat(utils): add utility functions

---
+++ apps/dashboard/server/utils/functions/createCallbackUrl.ts
@@ -0,0 +1,14 @@
+import { OAuth2Scopes } from "discord-api-types/v10";
+import { CLIENT_ID } from "#imports";
+
+const SCOPES = [
+	OAuth2Scopes.Identify,
+	OAuth2Scopes.Guilds,
+] as const;
+
+export function createCallbackUrl() {
+	const callbackUrl = encodeURIComponent(createCallbackUrl());
+	const scopes = encodeURIComponent(SCOPES.join(" "));
+
+	return `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${callbackUrl}&scope=${scopes}` as const;
+}
---
+++ apps/dashboard/server/utils/functions/createRedirectUrl.ts
@@ -0,0 +1,5 @@
+import { BASE_URL } from "#imports";
+
+export function createRedirectUrl() {
+	return `${BASE_URL}/api/auth/callback` as const;
+}
