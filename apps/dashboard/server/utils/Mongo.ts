e774b1bae52aab2f2667c13347e3b888a60e100f
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2025-12-26 21:25:57 +0100

    fix: top-level await is not allowed in `es2019`

--- apps/dashboard/server/utils/Mongo.ts
+++ apps/dashboard/server/utils/Mongo.ts
@@ -4,10 +4,14 @@ export const mongo = new MongoClient(MONGO_DB_CONNECTION_URL, {
 	maxPoolSize: 1,
 });
 
-await mongo
-	.connect()
-	.then(() => console.log('MongoDB Client is connected'))
-	.catch((error) => console.error(error));
+async function connect(): Promise<void> {
+	await mongo
+		.connect()
+		.then(() => console.log('MongoDB Client is connected'))
+		.catch((error) => console.error(error));
+}
+
+connect();
 
 export const db = mongo.db(MONGO_DB_DATABASE_NAME);
 export const collection = db.collection(MONGO_DB_COLLECTION_NAME);
