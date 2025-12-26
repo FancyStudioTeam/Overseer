70d42576b3831a949b4914a61b6e1dfa145911d7
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2025-12-26 21:23:43 +0100

    chore(Mongo): export `db` and `collection`

--- apps/dashboard/server/utils/Mongo.ts
+++ apps/dashboard/server/utils/Mongo.ts
@@ -8,3 +8,6 @@ await mongo
 	.connect()
 	.then(() => console.log('MongoDB Client is connected'))
 	.catch((error) => console.error(error));
+
+export const db = mongo.db(MONGO_DB_DATABASE_NAME);
+export const collection = db.collection(MONGO_DB_COLLECTION_NAME);
