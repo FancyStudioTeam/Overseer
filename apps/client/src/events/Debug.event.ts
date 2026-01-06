e8cd2e2bd05d5571db3e14fbe3d6feed02c7aff7
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-06 22:17:52 +0100

    chore: listen to `Debug` in `index.ts`

--- apps/client/src/events/Debug.event.ts
+++
@@ -1,9 +0,0 @@
-import { ClientEvents } from 'linkcord';
-import { defineEventConfig, type EventHandler } from 'linkcord/handlers';
-import { logger } from '#utils/Logger.js';
-
-export const config = defineEventConfig({
-	event: ClientEvents.Debug,
-});
-
-export const handler: EventHandler<typeof config> = ({ message }) => logger.debug(message);
--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,8 +1,10 @@
 import { loadEnvFile } from 'node:process';
-import { Client } from 'linkcord';
+import { Client, ClientEvents } from 'linkcord';
+import { logger } from '#utils/Logger.js';
 
 loadEnvFile();
 
 export const client = new Client();
 
+client.events.addEventListener(ClientEvents.Debug, (message) => logger.debug(message));
 client.init().catch(console.error);
