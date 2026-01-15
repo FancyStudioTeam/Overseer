ef2fa8c43e23d83796e0970e6f25fa7413e27a07
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-15 15:18:18 +0100

    fix: remove `use client`

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { headers } from 'next/headers';
 import { auth } from '#/lib/Auth.ts';
 
--- apps/dashboard/src/lib/Constants.ts
+++ apps/dashboard/src/lib/Constants.ts
@@ -1,3 +1,5 @@
+import 'server-only';
+
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
