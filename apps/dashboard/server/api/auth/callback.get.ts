36fc7151fe2c9373cba2f53bc931e934ade3ce12
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-15 04:48:54 +0100

    feat(dashboard): add `mongodb`

--- apps/dashboard/server/api/auth/callback.get.ts
+++ apps/dashboard/server/api/auth/callback.get.ts
@@ -1,4 +1,5 @@
 import { randomBytes } from 'node:crypto';
+import { sessions } from '~~/server/utils/MongoDB';
 
 const SESSION_ID_BYTES_LENGTH = 32;
 
@@ -6,6 +7,12 @@ export default defineEventHandler(async (event) => {
 	try {
 		const sessionId = randomBytes(SESSION_ID_BYTES_LENGTH).toString('base64url');
 		const { update } = await sessionManager(event);
+
+		await sessions.insertOne({
+			access_token: '123',
+			refresh_token: '123',
+			user_id: '123',
+		});
 
 		await update({
 			session_id: sessionId,
