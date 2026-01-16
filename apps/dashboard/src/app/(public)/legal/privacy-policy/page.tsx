aac9bf278cf53edd9d6dc4b26b8ab980f1011385
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-16 23:17:54 +0100

    chore: update legal pages

--- apps/dashboard/src/app/(public)/legal/privacy-policy/page.tsx
+++ apps/dashboard/src/app/(public)/legal/privacy-policy/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from 'next';
 import Content from './content.mdx';
+
+export const metadata: Metadata = {
+	title: 'Privacy Policy - Vanguard',
+};
 
 export default function () {
 	return (
@@ -6,13 +11,13 @@ export default function () {
 			<header className='grid h-50 place-content-center border-b bg-linear-to-br from-neutral-950 via-neutral-900 to-neutral-950 sm:h-75'>
 				<section className='flex max-w-xl flex-col gap-4 p-8 text-center sm:p-12'>
 					<h1 className='font-stardom text-3xl uppercase tracking-tighter sm:text-5xl'>Privacy Policy</h1>
-					<p className='hidden text-sm sm:block'>
+					<p className='hidden text-neutral-400 text-sm sm:block'>
 						This document describes the privacy policy and explains how data is collected, used, and protected when using our
 						service.
 					</p>
 				</section>
 			</header>
-			<main className='flex flex-col gap-4 p-8 text-md sm:p-12'>
+			<main className='flex flex-col gap-6 p-8 text-sm sm:p-12'>
 				<Content />
 			</main>
 		</>
--- apps/dashboard/src/app/(public)/legal/terms-of-service/page.tsx
+++ apps/dashboard/src/app/(public)/legal/terms-of-service/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from 'next';
 import Content from './content.mdx';
+
+export const metadata: Metadata = {
+	title: 'Terms of Service - Vanguard',
+};
 
 export default function () {
 	return (
@@ -6,13 +11,13 @@ export default function () {
 			<header className='grid h-50 place-content-center border-b bg-linear-to-br from-neutral-950 via-neutral-900 to-neutral-950 sm:h-75'>
 				<section className='flex max-w-xl flex-col gap-4 p-8 text-center sm:p-12'>
 					<h1 className='font-stardom text-3xl uppercase tracking-tighter sm:text-5xl'>Terms of Service</h1>
-					<p className='hidden text-sm sm:block'>
+					<p className='hidden text-neutral-400 text-sm sm:block'>
 						This document describes the terms and conditions of use and explains the rules you must follow when using our
 						service.
 					</p>
 				</section>
 			</header>
-			<main className='flex flex-col gap-4 p-8 text-md sm:p-12'>
+			<main className='flex flex-col gap-6 p-8 text-sm sm:p-12'>
 				<Content />
 			</main>
 		</>
