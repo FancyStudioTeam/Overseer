d70bd3eaa7266b46425d670fc2006cf0343b936e
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-16 22:26:08 +0100

    chore: update header style

--- apps/dashboard/src/app/(public)/legal/privacy-policy/page.tsx
+++ apps/dashboard/src/app/(public)/legal/privacy-policy/page.tsx
@@ -3,12 +3,12 @@ import Content from './content.mdx';
 export default function () {
 	return (
 		<>
-			<header className='grid h-50 place-content-center border-b bg-linear-to-t from-cyan-700 to-transparent sm:h-75'>
+			<header className='grid h-50 place-content-center border-b bg-linear-to-br from-neutral-950 via-neutral-900 to-neutral-950 sm:h-75'>
 				<section className='flex max-w-xl flex-col gap-4 p-8 text-center sm:p-12'>
-					<h1 className='font-extrabold text-3xl sm:text-5xl'>Privacy Policy</h1>
+					<h1 className='font-stardom text-3xl uppercase tracking-tighter sm:text-5xl'>Privacy Policy</h1>
 					<p className='hidden text-sm sm:block'>
-						This document describes the privacy policy and explains how data is collected, used, and protected in relation to
-						the use of our service.
+						This document describes the privacy policy and explains how data is collected, used, and protected when using our
+						service.
 					</p>
 				</section>
 			</header>
--- apps/dashboard/src/app/(public)/legal/terms-of-service/page.tsx
+++ apps/dashboard/src/app/(public)/legal/terms-of-service/page.tsx
@@ -3,11 +3,12 @@ import Content from './content.mdx';
 export default function () {
 	return (
 		<>
-			<header className='grid h-50 place-content-center border-b bg-linear-to-t from-cyan-700 to-transparent sm:h-75'>
+			<header className='grid h-50 place-content-center border-b bg-linear-to-br from-neutral-950 via-neutral-900 to-neutral-950 sm:h-75'>
 				<section className='flex max-w-xl flex-col gap-4 p-8 text-center sm:p-12'>
-					<h1 className='font-extrabold text-3xl sm:text-5xl'>Terms of Service</h1>
+					<h1 className='font-stardom text-3xl uppercase tracking-tighter sm:text-5xl'>Terms of Service</h1>
 					<p className='hidden text-sm sm:block'>
-						This document sets out the terms and conditions of use that you must follow with regard to the use of our service.
+						This document describes the terms and conditions of use and explains the rules you must follow when using our
+						service.
 					</p>
 				</section>
 			</header>
