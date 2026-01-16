8ffc9af1acc82f313fe9cf537f811fcd6f9b31bb
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-17 00:53:04 +0100

    chore: update landing page

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,32 +1,11 @@
-import { headers } from 'next/headers';
-import { LoginButton } from '#/components/Login.tsx';
-import { auth } from '#/lib/Auth.ts';
+import { Navbar } from '#/components/navbar/Navbar.tsx';
 
-export default async function Home() {
-	const session = await auth.api.getSession({
-		headers: await headers(),
-	});
-
-	if (!session) {
-		return <LoginButton />;
-	}
-
-	const { user } = session;
-	const { name } = user;
-
+export default function () {
 	return (
-		<main className='flex h-dvh w-full flex-col items-center justify-center gap-4'>
-			<h1 className='text-center font-extrabold text-2xl'>
-				Welcome back! <span className='font-black text-neutral-400'>{name}</span>
-			</h1>
-			<div className='w-80 flex-col gap-4 rounded-md border bg-neutral-900 p-6'>
-				<a
-					className='block w-full rounded-md bg-neutral-50 px-4 py-2 text-center text-neutral-950 text-sm'
-					href='/dashboard'
-				>
-					Dashboard
-				</a>
+		<div className='flex h-full min-h-dvh justify-center'>
+			<div className='w-full max-w-7xl xl:border-x'>
+				<Navbar />
 			</div>
-		</main>
+		</div>
 	);
 }
