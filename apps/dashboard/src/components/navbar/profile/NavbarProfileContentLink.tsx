669c80a7b46178425d6e631bfaad4fc6f5ee758e
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-19 18:04:01 +0100

    feat: add `ArrowSquareOutIcon` when `target` is `_blank`

--- apps/dashboard/src/components/navbar/profile/NavbarProfileContentLink.tsx
+++ apps/dashboard/src/components/navbar/profile/NavbarProfileContentLink.tsx
@@ -1,4 +1,4 @@
-import type { Icon } from '@phosphor-icons/react';
+import { ArrowSquareOutIcon, type Icon } from '@phosphor-icons/react';
 import Link from 'next/link';
 import type { HTMLAttributeAnchorTarget } from 'react';
 import { DropdownMenuItem } from '#/components/ui/Dropdown.tsx';
@@ -22,9 +22,16 @@ export function NavbarProfileContentLink({
 					weight='duotone'
 				/>
 				<span className='truncate'>{name}</span>
+				{isExternalLink(target) && (
+					<ArrowSquareOutIcon className='ml-auto size-5 shrink-0 text-neutral-400' />
+				)}
 			</Link>
 		</DropdownMenuItem>
 	);
+}
+
+function isExternalLink(target?: HTMLAttributeAnchorTarget): boolean {
+	return target === '_blank';
 }
 
 export interface NavbarProfileContentLinkProps {
