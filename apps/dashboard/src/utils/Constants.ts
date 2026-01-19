db1e47f8b9ab1d3463decc2580f27e20c9c0b3e9
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-19 23:27:18 +0100

    chore: use empty string for default value

--- apps/dashboard/src/utils/Constants.ts
+++ apps/dashboard/src/utils/Constants.ts
@@ -2,8 +2,8 @@ import 'server-only';
 
 import { createHash } from 'node:crypto';
 
-export const CLIENT_ID = process.env.CLIENT_ID;
-export const CLIENT_SECRET = process.env.CLIENT_SECRET;
+export const CLIENT_ID = process.env.CLIENT_ID ?? '';
+export const CLIENT_SECRET = process.env.CLIENT_SECRET ?? '';
 
 export const ENCRYPTION_ALGORITHM = 'aes-256-gcm';
 export const ENCRYPTION_HASH = createHash('sha256');
@@ -11,5 +11,5 @@ export const ENCRYPTION_IV_LENGTH = 12;
 export const ENCRYPTION_KEY = ENCRYPTION_HASH.update(process.env.ENCRYPTION_KEY ?? '').digest();
 export const ENCRYPTION_TAG_LENGTH = 16;
 
-export const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL;
-export const MONGO_DB_DATABASE_NAME = process.env.MONGO_DB_DATABASE_NAME;
+export const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL ?? '';
+export const MONGO_DB_DATABASE_NAME = process.env.MONGO_DB_DATABASE_NAME ?? '';
