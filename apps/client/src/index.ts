a9731c87481fee81a45b2899fd72bcdcb0e4eae8
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-22 23:15:05 +0100

    chore: update pm2 script for client

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,8 +1,9 @@
 import { loadEnvFile } from 'node:process';
 import { Client, ClientEvents } from 'linkcord';
+import { IS_PRODUCTION_ENVIRONMENT } from 'linkcord/utils';
 import { logger } from '#utils/Logger.js';
 
-loadEnvFile();
+loadEnvFile(IS_PRODUCTION_ENVIRONMENT ? '.env.prod' : '.env.dev');
 
 export const client = new Client();
 
