e028bc224b15872b0634d17ef543f46ac8ddc4ca
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-23 22:30:55 +0100

    feat: new HTTP status

--- apps/dashboard/src/lib/HTTPStatus.ts
+++ apps/dashboard/src/lib/HTTPStatus.ts
@@ -6,5 +6,8 @@ export const BAD_REQUEST_STATUS_TEXT = 'Bad Request';
 export const INTERNAL_SERVER_ERROR_STATUS_CODE = 500;
 export const INTERNAL_SERVER_ERROR_STATUS_TEXT = 'Internal Server Error';
 
+export const SEE_OTHER_STATUS_CODE = 303;
+export const SEE_OTHER_STATUS_TEXT = 'See Other';
+
 export const TOO_MANY_REQUESTS_STATUS_CODE = 429;
 export const TOO_MANY_REQUESTS_STATUS_TEXT = 'Too Many Requests';
