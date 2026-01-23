8533914190b8cc65d941f15a9aaa155919b67ea6
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-23 03:33:52 +0100

    feat: add `api/auth/sign-in`

---
+++ apps/dashboard/src/app/(API)/api/auth/sign-in/route.ts
@@ -0,0 +1,15 @@
+import { type NextRequest, NextResponse } from 'next/server';
+import { logger } from '#/lib/Logger.ts';
+import { createRedirectUrl } from '#/utils/functions/createRedirectUrl.ts';
+
+export function POST(_: NextRequest) {
+	try {
+		return NextResponse.redirect(createRedirectUrl());
+	} catch (error) {
+		logger.error(error);
+
+		return NextResponse.json({
+			success: false,
+		});
+	}
+}
