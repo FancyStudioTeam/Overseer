efc79ddff1972793e9ebe85e3004ed9f22bfa7e2
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-23 04:55:55 +0100

    chore: set `oauth2_state`

--- apps/dashboard/src/app/(API)/api/auth/sign-in/route.ts
+++ apps/dashboard/src/app/(API)/api/auth/sign-in/route.ts
@@ -1,10 +1,21 @@
+import { randomBytes } from 'node:crypto';
+import { cookies as NextCookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { logger } from '#/lib/Logger.ts';
 import { createRedirectUrl } from '#/utils/functions/createRedirectUrl.ts';
 
-export function POST() {
+const OAUTH2_STATE_BYTES_LENGTH = 24;
+
+export async function POST() {
 	try {
-		return NextResponse.redirect(createRedirectUrl());
+		const cookies = await NextCookies();
+
+		const oauth2State = randomBytes(OAUTH2_STATE_BYTES_LENGTH);
+		const oauth2StateString = oauth2State.toString('utf-8');
+
+		await cookies.set('oauth2_state', oauth2StateString);
+
+		return NextResponse.redirect(createRedirectUrl(oauth2StateString));
 	} catch (error) {
 		logger.error(error);
 
