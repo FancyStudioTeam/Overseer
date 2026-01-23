b33ec40e9ede6704bdaf24b769d15bd76b2f6040
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-23 05:09:04 +0100

    chore: things

--- apps/dashboard/src/utils/functions/createRedirectUrl.ts
+++ apps/dashboard/src/utils/functions/createRedirectUrl.ts
@@ -1,8 +1,8 @@
-import { OAuth2Scopes, Routes } from 'discord-api-types/v10';
+import { OAuth2Routes, OAuth2Scopes } from 'discord-api-types/v10';
 import { CLIENT_ID } from '#/lib/Constants.ts';
 import { createCallbackUrl } from './createCallbackUrl.ts';
 
-const { oauth2Authorization } = Routes;
+const { authorizationURL } = OAuth2Routes;
 
 const SCOPES = [
 	OAuth2Scopes.Email,
@@ -10,15 +10,22 @@ const SCOPES = [
 	OAuth2Scopes.Guilds,
 ];
 
-export function createRedirectUrl(state: string) {
-	const redirectUrl = new URL(oauth2Authorization(), 'https://discord.com');
-	const { searchParams } = redirectUrl;
+export function createRedirectUrl(state: string): string {
+	const callbackUrl = createCallbackUrl();
+	const scopesString = SCOPES.join(' ');
+
+	const encodedScopes = encodeURIComponent(scopesString);
+	const encodedState = encodeURIComponent(btoa(state));
+	const encodedRedirectUri = encodeURIComponent(callbackUrl);
+
+	const authorizationUrl = new URL(authorizationURL);
+	const { searchParams } = authorizationUrl;
 
 	searchParams.append('client_id', CLIENT_ID);
 	searchParams.append('response_type', 'code');
-	searchParams.append('redirect_uri', encodeURIComponent(createCallbackUrl()));
-	searchParams.append('scope', encodeURIComponent(SCOPES.join(' ')));
-	searchParams.append('state', encodeURIComponent(btoa(state)));
+	searchParams.append('redirect_uri', encodedRedirectUri);
+	searchParams.append('scope', encodedScopes);
+	searchParams.append('state', encodedState);
 
-	return redirectUrl.toString();
+	return authorizationUrl.toString();
 }
