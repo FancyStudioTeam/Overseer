d5116a59bc062fea332a9dbc00776ee7f8dbce84
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-23 05:01:40 +0100

    chore: add `REST`

--- apps/dashboard/src/lib/Constants.ts
+++ apps/dashboard/src/lib/Constants.ts
@@ -8,6 +8,7 @@ export const {
 	BASE_URL = 'http://localhost:3000',
 	CLIENT_ID = '1234567890987654321',
 	CLIENT_SECRET = 'youshallnopass',
+	CLIENT_TOKEN = 'xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx',
 	ENCRYPTION_KEY = 'youshallnopass',
 	MONGO_DB_COLLECTION_NAME = 'sessions',
 	MONGO_DB_CONNECTION_URL = 'mongodb://localhost:27017',
---
+++ apps/dashboard/src/lib/REST.ts
@@ -0,0 +1,10 @@
+import 'server-only';
+
+import { REST } from '@discordjs/rest';
+import { CLIENT_TOKEN } from './Constants.ts';
+
+export const rest = new REST({
+	version: '10',
+});
+
+rest.setToken(CLIENT_TOKEN);
