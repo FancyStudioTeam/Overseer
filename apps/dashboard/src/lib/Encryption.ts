52d75fa36455f3f8d1221ac178d9f1146a5067ca
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-23 03:14:45 +0100

    refactor: use static class for encryption

---
+++ apps/dashboard/src/lib/Encryption.ts
@@ -0,0 +1,63 @@
+import 'server-only';
+
+import { createCipheriv, createDecipheriv, randomBytes } from 'node:crypto';
+import {
+	ENCRYPTION_ALGORITHM,
+	ENCRYPTION_IV_LENGTH,
+	ENCRYPTION_SECRET,
+	ENCRYPTION_TAG_LENGTH,
+} from './Constants.ts';
+
+export class Encryption {
+	static decrypt(encryptedData: string): string {
+		const encryptedBufferData = Buffer.from(encryptedData, 'base64');
+
+		const iv = encryptedBufferData.subarray(0, ENCRYPTION_IV_LENGTH);
+		const authTag = encryptedBufferData.subarray(
+			ENCRYPTION_IV_LENGTH,
+			ENCRYPTION_IV_LENGTH + ENCRYPTION_TAG_LENGTH,
+		);
+
+		const encrypted = encryptedBufferData.subarray(
+			ENCRYPTION_IV_LENGTH + ENCRYPTION_TAG_LENGTH,
+		);
+		const decipher = createDecipheriv(ENCRYPTION_ALGORITHM, ENCRYPTION_SECRET, iv);
+
+		decipher.setAuthTag(authTag);
+
+		const decrypted = decipher.update(encrypted);
+		const final = decipher.final();
+
+		const decryptedBufferData = Buffer.concat([
+			decrypted,
+			final,
+		]);
+		const decryptedBufferString = decryptedBufferData.toString('utf-8');
+
+		return decryptedBufferString;
+	}
+
+	static encrypt(unencryptedData: string): string {
+		const iv = randomBytes(ENCRYPTION_IV_LENGTH);
+		const cipher = createCipheriv(ENCRYPTION_ALGORITHM, ENCRYPTION_SECRET, iv);
+
+		const encrypted = cipher.update(unencryptedData, 'utf-8');
+
+		const final = cipher.final();
+		const authTag = cipher.getAuthTag();
+
+		const encryptedBuffer = Buffer.concat([
+			encrypted,
+			final,
+		]);
+
+		const encryptedBufferData = Buffer.concat([
+			iv,
+			authTag,
+			encryptedBuffer,
+		]);
+		const encryptedBufferString = encryptedBufferData.toString('base64');
+
+		return encryptedBufferString;
+	}
+}
