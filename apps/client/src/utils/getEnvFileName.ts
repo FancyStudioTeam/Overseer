71d02c8547cb58795898208149f6141792b599cb
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-25 03:08:43 +0100

    docs: add TSDoc to some functions

--- apps/client/src/utils/getEnvFileName.ts
+++ apps/client/src/utils/getEnvFileName.ts
@@ -1,5 +1,14 @@
 import { IS_PRODUCTION_ENVIRONMENT } from 'linkcord/utils';
 
+/**
+ * Gets the correct `.env` file depending on the `IS_PRODUCTION_ENVIRONMENT`
+ * constant from Linkcord.
+ *
+ * @remarks
+ * Linkcord will return either `production` or `development` from `NODE_ENV`.
+ *
+ * If `NODE_ENV` is not set, `production` will be returned as default.
+ */
 export function getEnvFileName() {
 	if (IS_PRODUCTION_ENVIRONMENT) {
 		return '.env.production' as const;
--- apps/client/src/utils/getMemoryUsage.ts
+++ apps/client/src/utils/getMemoryUsage.ts
@@ -1,8 +1,10 @@
 import { memoryUsage } from 'node:process';
 
 const CONVERSION_FACTOR = 1_024;
-const BYTES_IN_MEGABYTES = CONVERSION_FACTOR * CONVERSION_FACTOR;
 
+/**
+ * Gets the formatted memory usage (in megabytes) as a string from V8.
+ */
 export function getMemoryUsage() {
 	const { heapUsed } = memoryUsage();
 	const formattedMegaBytes = formatToMegaBytes(heapUsed);
@@ -10,6 +12,11 @@ export function getMemoryUsage() {
 	return `${formattedMegaBytes} MB` as const;
 }
 
+/**
+ * Formats the provided bytes into megabytes.
+ *
+ * @param bytes - The bytes to convert into megabytes.
+ */
 function formatToMegaBytes(bytes: number): string {
-	return (bytes / BYTES_IN_MEGABYTES).toFixed(2);
+	return (bytes / (CONVERSION_FACTOR * CONVERSION_FACTOR)).toFixed(2);
 }
