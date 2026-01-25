002ea63bf823a4e9a0c8aad6bb76b32b58807ac6
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-25 17:00:09 +0100

    chore: delete TypeORM

--- apps/client/src/db/repositories/GuildSettingsRepository.ts
+++
@@ -1,4 +0,0 @@
-import { AppDataSource } from '#db/DataSource.js';
-import { GuildSettingsModel } from '#db/models/GuildSettingsModel.js';
-
-export const GuildSettingsRepository = AppDataSource.getMongoRepository(GuildSettingsModel);
