930949aeb21f37084690aef5995ac607df360f92
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-26 22:43:45 +0100

    docs: add comments to functions

--- apps/dashboard/src/utils/createCallbackUrl.ts
+++ apps/dashboard/src/utils/createCallbackUrl.ts
@@ -1,5 +1,9 @@
 import { BASE_URL } from '#/lib/Constants.ts';
 
+/**
+ * Creates a callback URL where the users will be redirected after they
+ * authorize the application.
+ */
 export function createCallbackUrl() {
 	return `${BASE_URL}/api/auth/callback` as const;
 }
--- apps/dashboard/src/utils/createRedirectUrl.ts
+++ apps/dashboard/src/utils/createRedirectUrl.ts
@@ -13,26 +13,37 @@ const SCOPES = [
 	OAuth2Scopes.GuildsMembersRead,
 ];
 
+/**
+ * Creates a redirect URL to the application authorization page with the
+ * specified options.
+ */
 export function createRedirectUrl(state: string): string {
 	const callbackUrl = createCallbackUrl();
+
+	/**
+	 * NOTE: Join Discord scopes with '+'.
+	 */
 	const scopesString = SCOPES.join('+');
 
 	const authorizationUrl = new URL(authorizationURL);
 	const { searchParams } = authorizationUrl;
 
 	/*
-	 * When using `URLSearchParams` methods, their value will be encoded.
-	 *
-	 * Discord requires the scopes to be joined with a '+'.
+	 * When using 'URLSearchParams' methods, their value will be encoded.
 	 *
 	 * This assignment MUST be made before setting other query string parameters,
-	 * as this property overrides all search properties
+	 * as this property overrides all search properties.
 	 */
 	authorizationUrl.search = `scope=${scopesString}`;
 
 	searchParams.set('client_id', CLIENT_ID);
 	searchParams.set('redirect_uri', callbackUrl);
 	searchParams.set('response_type', 'code');
+
+	/*
+	 * Set 'prompt' to 'none' to avoid the user from clicking 'Authorize' when they
+	 * already previously authorized the application with the specified scopes.
+	 */
 	searchParams.set('prompt', 'none');
 	searchParams.set('state', state);
 
