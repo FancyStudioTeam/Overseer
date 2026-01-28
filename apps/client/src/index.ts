106b9070c899ad7867e10eea68976feaa043ec66
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-28 01:42:04 +0100

    chore: delete `getErrorMessage`

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -2,7 +2,6 @@ import { loadEnvFile } from 'node:process';
 import { Client, ClientEvents } from 'linkcord';
 import { logger } from '#lib/Logger.js';
 import { getEnvFileName } from '#utils/getEnvFileName.js';
-import { getErrorMessage } from '#utils/getErrorMessage.js';
 
 /*
  * Load the correct `.env` file based on `NODE_ENV`.
@@ -23,4 +22,4 @@ client.events.addEventListener(ClientEvents.Debug, (message) => logger.debug(mes
 client
 	.init()
 	.then(() => logger.info('Client has been initialized'))
-	.catch((error) => logger.error(getErrorMessage(error)));
+	.catch((error) => logger.error('Error while initializing the client:\n\t', error));
