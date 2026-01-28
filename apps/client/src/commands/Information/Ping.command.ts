14fdbc3acf6d7dad00e6fb4b605b5baf7a6d3311
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-28 01:23:45 +0100

    refactor: `Ping.command`

--- apps/client/src/commands/Information/Ping.command.ts
+++ apps/client/src/commands/Information/Ping.command.ts
@@ -2,11 +2,11 @@ import type { GatewayManager, GatewayShard } from 'linkcord';
 import { ContainerBuilder, TextDisplayBuilder } from 'linkcord/builders';
 import { type ChatInputCommandContext, ChatInputCommandHandler, Declare } from 'linkcord/handlers';
 import { MessageFlags } from 'linkcord/types';
-import { bold, CodeBlockLanguage, type Collection, codeBlock } from 'linkcord/utils';
-import { formatKeyValues } from '#utils/formatKeyValues.js';
+import type { Collection } from 'linkcord/utils';
+import { formatTextDisplayContent } from '#utils/formatTextDisplayContent.js';
 
 @Declare({
-	description: 'Displays the latency of the bot',
+	description: 'Displays the latency of all Vanguard gateway shards',
 	name: 'ping',
 })
 export default class extends ChatInputCommandHandler {
@@ -22,13 +22,12 @@ export default class extends ChatInputCommandHandler {
 		});
 	}
 
-	private buildGatewayShardsBuilder({ shards }: GatewayManager): TextDisplayBuilder {
-		const formattedShardsPairs = this.formatGatewayShards(shards);
+	private createGatewayShardsBuilder({ shards }: GatewayManager): TextDisplayBuilder {
+		const gatewayShardsListPairs = this.formatGatewayShardsListPairs(shards);
 
 		const textDisplayBuilder = new TextDisplayBuilder();
 		const textDisplayBuilderContent = [
-			bold('Gateway Shards'),
-			codeBlock(CodeBlockLanguage.ANSI, formatKeyValues(formattedShardsPairs)),
+			formatTextDisplayContent(gatewayShardsListPairs),
 		];
 
 		textDisplayBuilder.setContent(textDisplayBuilderContent.join('\n'));
@@ -38,7 +37,7 @@ export default class extends ChatInputCommandHandler {
 
 	private createMainContainerBuilder(gatewayManager: GatewayManager): ContainerBuilder {
 		const containerBuilder = new ContainerBuilder();
-		const gatewayShardsBuilder = this.buildGatewayShardsBuilder(gatewayManager);
+		const gatewayShardsBuilder = this.createGatewayShardsBuilder(gatewayManager);
 
 		containerBuilder.addComponents([
 			gatewayShardsBuilder,
@@ -47,20 +46,11 @@ export default class extends ChatInputCommandHandler {
 		return containerBuilder;
 	}
 
-	private formatGatewayShards(shards: Collection<number, GatewayShard>): [
-		Key: string,
-		Value: string,
-	][] {
+	private formatGatewayShardsListPairs(shards: Collection<number, GatewayShard>): string[] {
 		const shardsArray = shards.toArray();
-		const shardsArrayPairs = shardsArray.map<
-			[
-				Key: string,
-				Value: string,
-			]
-		>(({ id, latency }) => [
-			`Gateway Shard #${id}`,
-			`${latency}ms`,
-		]);
+		const shardsArrayPairs = shardsArray.map(
+			({ id, latency }) => `Gateway Shard #${id} » ${latency}ms`,
+		);
 
 		return shardsArrayPairs;
 	}
