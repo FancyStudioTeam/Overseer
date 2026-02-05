b20151c1f3fce302a735cf140da8d033d40f6f3a
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-02-05 13:34:30 +0100

    docs: things

--- apps/client/src/lib/Logger.ts
+++ apps/client/src/lib/Logger.ts
@@ -44,6 +44,7 @@ const LOGGER_LEVEL_COLORS: Record<LoggerLevels, string> = {
 
 /*
  * Winston levels are sorted from most (0) to least severe.
+ *
  * Reference: https://github.com/winstonjs/winston?tab=readme-ov-file#logging
  */
 const LOGGER_LEVELS = {
