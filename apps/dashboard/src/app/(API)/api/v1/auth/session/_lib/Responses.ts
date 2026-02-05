9eb3766acb8ca350a9dd63be9a71582ccda89571
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-02-05 14:03:14 +0100

    chore: use JWT for user

--- apps/dashboard/src/app/(API)/api/v1/auth/session/_lib/Responses.ts
+++ apps/dashboard/src/app/(API)/api/v1/auth/session/_lib/Responses.ts
@@ -5,14 +5,18 @@
 
 import 'server-only';
 
-import { NextResponse } from 'next/server';
+import type { Snowflake } from 'discord-api-types/v10';
+import type { NextResponse } from 'next/server';
 import {
 	INTERNAL_SERVER_ERROR_STATUS_CODE,
 	INTERNAL_SERVER_ERROR_STATUS_TEXT,
+	OK_STATUS_CODE,
+	OK_STATUS_TEXT,
 	UNAUTHORIZED_STATUS_CODE,
 	UNAUTHORIZED_STATUS_TEXT,
 } from '#/lib/HTTPStatus.ts';
 import { createErrorJsonResponse } from '#/utils/createErrorJsonResponse.ts';
+import { createJsonResponse } from '#/utils/createJsonResponse.ts';
 
 export function INTERNAL_SERVER_ERROR_RESPONSE(): NextResponse {
 	return createErrorJsonResponse(
@@ -25,15 +29,25 @@ export function INTERNAL_SERVER_ERROR_RESPONSE(): NextResponse {
 	);
 }
 
+export function SESSION_RESPONSE(user: SessionResponseUser): NextResponse {
+	return createJsonResponse(OK_STATUS_CODE, OK_STATUS_TEXT, {
+		user,
+	});
+}
+
 export function UNAUTHORIZED_RESPONSE(): NextResponse {
-	return NextResponse.json(
+	return createErrorJsonResponse(
+		UNAUTHORIZED_STATUS_CODE,
+		UNAUTHORIZED_STATUS_TEXT,
 		{
-			data: null,
-			success: true,
-		},
-		{
-			status: UNAUTHORIZED_STATUS_CODE,
-			statusText: UNAUTHORIZED_STATUS_TEXT,
+			code: 'UNAUTHORIZED',
+			message: 'You cannot perform this operation without authentication',
 		},
 	);
+}
+
+interface SessionResponseUser {
+	avatar: string | null;
+	id: Snowflake;
+	name: string;
 }
--- apps/dashboard/src/app/(API)/api/v1/auth/session/route.ts
+++ apps/dashboard/src/app/(API)/api/v1/auth/session/route.ts
@@ -1,11 +1,9 @@
 import { cookies as NextCookies } from 'next/headers';
-import { type NextRequest, NextResponse } from 'next/server';
-import { sessionsCollection } from '#/lib/auth/MongoDB.ts';
-import { OK_STATUS_CODE, OK_STATUS_TEXT } from '#/lib/HTTPStatus.ts';
+import type { NextRequest } from 'next/server';
 import { Jose } from '#/lib/Jose.ts';
 import { logger } from '#/lib/Logger.ts';
 import { INTERNAL_SERVER_ERROR_RESPONSE } from '../sign-in/_lib/Responses.ts';
-import { UNAUTHORIZED_RESPONSE } from './_lib/Responses.ts';
+import { SESSION_RESPONSE, UNAUTHORIZED_RESPONSE } from './_lib/Responses.ts';
 
 export async function GET(nextRequest: NextRequest) {
 	try {
@@ -17,39 +15,15 @@ export async function GET(nextRequest: NextRequest) {
 		}
 
 		const { value } = sessionCookie;
-		const sessionId = await Jose.verify(value);
+		const jsonWebTokenPayload = await Jose.verify(value);
 
-		if (!sessionId) {
+		if (!jsonWebTokenPayload) {
 			return UNAUTHORIZED_RESPONSE();
 		}
 
-		const session = await sessionsCollection.findOne({
-			session_id: sessionId,
-		});
+		const { user } = jsonWebTokenPayload;
 
-		if (!session) {
-			nextCookies.delete('session');
-
-			return UNAUTHORIZED_RESPONSE();
-		}
-
-		const { user } = session;
-		const { avatar, id, name } = user;
-
-		return NextResponse.json(
-			{
-				data: {
-					avatar,
-					id,
-					name,
-				},
-				success: true,
-			},
-			{
-				status: OK_STATUS_CODE,
-				statusText: OK_STATUS_TEXT,
-			},
-		);
+		return SESSION_RESPONSE(user);
 	} catch (error) {
 		const { nextUrl } = nextRequest;
 		const { href } = nextUrl;
